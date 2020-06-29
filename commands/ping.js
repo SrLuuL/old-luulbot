be431cfb9b6643327a714a60c6d5529f13ee3a4b
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-06-29 13:48:20 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,17 @@
+const si = require("systeminformation");
+
 module.exports.run = (client, message, args, username, channel) => {
-	
-	client.say(channel, "teste");
+
+
+
+    let total = process.memoryUsage().heapTotal / 1024 / 1024;
+    let realtotal = Math.round(total * 100) / 100;
+
+    client.ping().then(data => {
+      let ping = Math.floor(Math.round(data * 1000));
+
+      client.say(channel, `Ping: ${ping} ms || luul.glitch.me/ || Memoria: ${realtotal}MB/512MB || Comandos usados: ${comandosx}`);
+    });
 	
 }
 
