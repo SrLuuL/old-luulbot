17f0cdb86ecc3b7bae7623b9a16e5832245e5072
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-01 19:51:08 -0300

    Update modstats.js

--- commands/modstats.js
+++ commands/modstats.js
@@ -21,7 +21,7 @@ for(let x = 0; x < num; x++) {
 list.push(channels[x].name)
 }
   
-const haste = hastebin(list, {extension: "txt"})  
+const haste = await hastebin(list, {extension: "txt"})  
 
 let data2 = await req2.json()
 let user = data2.user
