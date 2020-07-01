adfacf170715e852524a523507632eeca9483dcb
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-06-30 22:54:51 -0300

    Update hora.js

--- commands/hora.js
+++ commands/hora.js
@@ -1,4 +1,4 @@
-module.exports.run = (client, message, args, username, channel) => {
+module.exports.run = async (client, message, args, username, channel) => {
 
 if (!args[0]) {
 return client.say(channel, `${username}, insira um país :/`)
@@ -18,11 +18,14 @@ let req = unirest("GET", `http://worldtimeapi.org/api/timezone/${country}`)
 
 req.end(function (res){
 
- if (res.body.error == "unknown location") {
-  return client.say(channel, `${username}, local inválido :/`)
- }
+
   
-let datetime = res.body.datetime
+let datetime = await res.body.datetime
+
+if datetime === 0 {
+ return client.say(channel, `${username}, local inválido :/`)
+}
+
 let date = datetime.slice(11, 19)
 
 client.say(channel, `${username}, são ${date} em ${args[0]} agora.`)
