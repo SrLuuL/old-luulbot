7fe19d86220afcdf9cd9a2633dbf2d0a70e4a823
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-02 14:51:07 -0300

    Create canalinfo.js

---
+++ commands/canalinfo.js
@@ -0,0 +1,26 @@
+module.exports.run = async (client, message, args, username, channel) => {
+
+const fetch = require("node-fetch")
+
+let sender = (!args[0]) ? username : args[0]
+
+let res = await fetch(`https://api.ivr.fi/twitch/resolve/${sender}`);
+let data = await res.json();
+
+let user = data.displayName
+let userid = data.id
+let bio = data.bio
+bio = (!bio) ? "(sem bio)" : bio
+let color = data.chatColor
+let banned = data.banned
+banned = (banned === true) ? `${user} está banido` : `${user} não está banido`
+let partner = data.partner
+partner = (partner === true) ? "sim" : "não"
+
+if (args[1] == --b) {
+return client.say(channel, `${username}, ${banned}`)
+}
+
+client.say(channel, `${username}, Canal: ${user} | ID: ${userid} | Bio: ${bio} | Cor: ${color}`)
+
+}
