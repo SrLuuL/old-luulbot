f4fb565b7cb852a40d4d10eebe5a8cb018126dee
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-02 15:17:45 -0300

    Update canalinfo.js

--- commands/canalinfo.js
+++ commands/canalinfo.js
@@ -15,11 +15,26 @@ let color = data.chatColor
 let banned = data.banned
 banned = (banned === true) ? `${user} está banido` : `${user} não está banido`
 let partner = data.partner
-partner = (partner === true) ? "sim" : "não"
-
-if (args[1] == "--b") {
-return client.say(channel, `${username}, ${banned}`)
+partner = (partner === true) ? `${user} é um parceiro. ` : ""
+let aff = data.affiliate
+aff = (aff === true) ? `${user} é um afiliado. ` : ""
+let staff = data.roles.isStaff
+staff = (staff === true) ? `${user} é um staff. ` : ""
+let roles = aff + partner + staff
+if (data.affiliate && data.partner && data.roles.isStaff) === false {
+  roles = `${user} não  possui cargos`
 }
+  
+
+  
+  switch (args[1]) {
+    case "--b":
+      client.say(channel, `${username}, ${banned}`)
+      break;
+    case "--c":
+      client.say(channel, `${username}, ${roles}`)
+      break;
+  }
 
 client.say(channel, `${username}, Canal: ${user} | ID: ${userid} | Bio: ${bio} | Cor: ${color}`)
 
