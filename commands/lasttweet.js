9e95cff195f5552d76e49bd0c2230283be921d25
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-02 14:16:08 -0300

    Update lasttweet.js

--- commands/lasttweet.js
+++ commands/lasttweet.js
@@ -1,14 +1,16 @@
 module.exports.run = async (client, message, args, username, channel) => {
 
-const fetch = require("node-fetch")
+const unirest = require("unirest")
 
-let res = await fetch(`https://decapi.me/twitter/latest/${args[0]}?howlong&no_rts&lang=pt&precision=2`)
+let res = await unirest("GET", `https://decapi.me/twitter/latest/${args[0]}?howlong&no_rts&lang=pt&precision=2`)
 let tweet = await res.body
+tweet = tweet.replace(/ago/g, "atrás")
+
 
 if (!args[0]){
 return client.say(channel, `${username}, insira um usuário :/`)
 } else {
-client.say(channel, `${res}`)
+client.say(channel, `${tweet}`)
 }
 
 }
