45dff0b5e36f25cd9cede666f42e70b34a71b91c
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-02 14:04:04 -0300

    Create lasttweet.js

---
+++ commands/lasttweet.js
@@ -0,0 +1,22 @@
+module.exports.run = async (client, message, args, username, channel) => {
+
+const fetch = require("node-fetch")
+
+let res = await fetch(`https://decapi.me/twitter/latest/${args[0]}?howlong&no_rts&lang=pt&precision=2`)
+res = res.replace(/ago/g, "atrás")
+
+if (!args[0]){
+return client.say(channel, `${username}, insira um usuário :/`)
+} else {
+client.say(channel, `${res}`)
+}
+
+}
+
+module.exports.config = {
+name: "lasttweet",
+aliases: ["lt"],
+description: "Pega um último tweet de um usuário do Twitter",
+usage: "lasttweet [user]"
+}
+
