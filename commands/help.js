c0e527bb6012c6c8f9e52b995252face769d88b1
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-02 22:55:56 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ else {
 let command = cmd.get(alias.get(args[0].toLowerCase()) || args[0].toLowerCase());
 if (!command) return client.say(channel, `${username}, não possuo esse comando :/ `)
 command = command.config
-client.say(channel, `${username}, * ${command.usage} : ${command.description}. Apelidos: (${command.aliases.join(", ")})`)
+client.say(channel, `${username}, * ${command.usage}(*${command.aliases.join("/*")})( : ${command.description}.`)
 }
 }
   
