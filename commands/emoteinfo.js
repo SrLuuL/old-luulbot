da8c74ffc29510a55bc21754d31050f6f41cf4ad
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-02 22:39:16 -0300

    Create emoteinfo.js

---
+++ commands/emoteinfo.js
@@ -0,0 +1,28 @@
+module.exports.run = async (client, message, args, username, channel) => {
+
+const fetch = require("node-fetch")
+
+let res = await fetch(`https://api.ivr.fi/twitch/emotes/${args[0]}`);
+let data = res.json();
+
+let user = data.channel
+let id = data.channelid
+let emote = data.emotecode
+let tier = data.tier
+let url = data.emoteurl_3x
+
+if (!args[0]) {
+return client.say(channel, `${username}, insira um emote :/`)
+} else {
+client.say(channel, `${username}, Emote: ${emote} | ID: ${id} | Canal: ${user} | Tier: ${tier} | ${url}`)
+}
+
+}
+
+module.exports.config = {
+name: "emoteinfo",
+aliases: ["ei"],
+description: "Mostra informações de um emote de inscrito",
+usage: "emoteinfo [emote]"
+}
+
