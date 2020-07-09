26865354fd537bb12250e2a18c0d39f34e09bcdc
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-09 14:59:53 -0300

    Update login.js

--- credentials/login.js
+++ credentials/login.js
@@ -15,7 +15,7 @@ const options = {
     username: "LuuLBot",
     password: "oauth:4lcd5inaptc5tncqsumrugzxro2p6l",
   },
-  channels: ["srluul", "bhaskaraii", "namaca", "widepeeposaddankcomfysip",
+  channels: ["srluul", "bhaskarai", "namaca", "widepeeposaddankcomfysip",
   "ellpspsps", "kkalfoy", "nekolk", "keert"]
 };
   
