85266907be04466ade22b4de5a869fb70c6fbc19
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-11 19:47:53 -0300

    Update subage.js

--- commands/subage.js
+++ commands/subage.js
@@ -8,16 +8,38 @@ module.exports.run = async (client, message, args, username, channel) => {
  if (!args[0] && !args[1]) {
 user1 = username;
 user2 = sender;
- }else!args[1]) {
+ } else if (!args[1]) {
  user1 = username;
  user2 = args[0];
+ } else {
+   user1 = args[0];
+   user2 = args[1];
  }
    
- let res = await fetch(`https://api.ivr.fi/twitch/subage/${username}/${sender}`)
+ let res = await fetch(`https://api.ivr.fi/twitch/subage/${user1}/${user2}`)
  let data = await res.json();
  
+ if (data.status === 404) {
+   return client.say(channel, `${username}, usuário não foi encontrado :/`)
+ }
 
- 
+  
+  let canal = data.channel
+  let subCumulative = data.cumulative.months
+  let endCumulative = data.cumulative.remaining
+  let user = data.username
+  const {subscribed} = data;
+  const {type, tier} = data.meta;
+  const {remaining} = data.streak.remaining;
+  type = type.replace("paid", "pago");
+  
+  if (subscribed === false) {
+    client.say(channel, `${username}, ${user} não é inscrito em ${canal}`)
+} else {
+  client.say(channel, `${username}, ${user} é inscrito em ${canal} há ${subCumulative} | Sub acaba em: ${remaining} | Próximo aniversário em: ${endCumulative} | Sub: ${type.toLowerCase()} | Tier: ${tier} `) 
+}
+  
+  
 }
 
 
