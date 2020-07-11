513364725319702586c81c0ddca18c6a22f86c58
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-10 23:48:47 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,16 @@
 module.exports.run = (client, message, args, username, channel, cmd, alias) => {
 
 if (!args[0]) {
-client.say(channel, `${username}, Prefixo: * | Comandos: https://luul.glitch.me/comandos | `)
-}
-else {
+ let commands = cmd.map(c => `${c.config.name}`).join(", ")
+  
+client.say(channel, `${username}, Prefixo: * | Comandos: ${commands} | `)
+} else {
 let command = cmd.get(alias.get(args[0].toLowerCase()) || args[0].toLowerCase());
 if (!command) return client.say(channel, `${username}, não possuo esse comando :/ `)
 command = command.config
 client.say(channel, `${username}, *${command.name}(*${command.aliases.join("/*")}): ${command.description}.`)
 }
+  
 }
   
 module.exports.config = {
