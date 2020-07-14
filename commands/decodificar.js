c068f3bfac35a996e335b19f8131762632f0dcfb
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-14 19:46:10 -0300

    Create decodificar.js

---
+++ commands/decodificar.js
@@ -0,0 +1,23 @@
+module.exports.run = async (client, message, args, username, channel) => {
+
+const fetch = require('node-fetch')
+
+if (!args[0] || !args[1]) return client.say(channel, `${username}, insira uma criptografia e texto :/`)
+
+let resMorse = await fetch(`http://www.morsecode-api.de/decode?string=${args.join(" ").slice(args[0].length)
+let dataMorse = await resMorse.json();
+let textMorse = dataMorse.plaintext
+
+switch(args[0]) {
+case morse:
+return client.say(channel, `${username}, ${textMorse}`)
+}
+
+}
+
+module.exports.config = {
+name: "decodificar",
+aliases: ["dc"],
+description: "decodifica um texto de uma criptografia",
+usage: "decodificar [cript] [text]"
+}
