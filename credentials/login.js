a1ef1aa1a1fad69ffc92a8dda79c9d157c798e1d
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-14 14:21:36 -0300

    Update login.js

--- credentials/login.js
+++ credentials/login.js
@@ -13,7 +13,7 @@ const options = {
     username: "LuuLBot",
     password: "oauth:4lcd5inaptc5tncqsumrugzxro2p6l",
   },
-  channels: ["srluul", dbchannels.channels]
+  channels: ["srluul", `${dbchannels.channels}`]
 };
   
 module.exports = { options }
