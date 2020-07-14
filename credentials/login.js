e1f5a7db2470054aaa5572c87bffa795f2071913
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-14 15:01:31 -0300

    Update login.js

--- credentials/login.js
+++ credentials/login.js
@@ -1,6 +1,6 @@
 const db = require("quick.db")
-db.set('channels', {channels: [""]} )
-const dbchannels = db.get('channels.channels')  
+db.set('channels', [''])
+const dbchannels = db.get('channels')  
   
 const options = {
   options: {
