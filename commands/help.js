087795b4b47ce758f53df4f36ce53cdf610b9236
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-14 19:29:40 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,12 @@ module.exports.run = (client, message, args, username, channel, cmd, alias) => {
 if (!args[0]) {
  let commands = cmd.map(c => `${c.config.name}`).join(", ")
   
-client.say(channel, `${username}, Prefixo: * | Comandos: ${commands} | `)
+client.say(channel, `${username}, Prefixo: = | Comandos: ${commands} | `)
 } else {
 let command = cmd.get(alias.get(args[0].toLowerCase()) || args[0].toLowerCase());
 if (!command) return client.say(channel, `${username}, não possuo esse comando :/ `)
 command = command.config
-client.say(channel, `${username}, *${command.name}(*${command.aliases.join("/*")}): ${command.description}.`)
+client.say(channel, `${username}, =${command.name}(*${command.aliases.join("/=")}): ${command.description}.`)
 }
   
 }
