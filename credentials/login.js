a385c8fa3c20314826918f459a3fb2d791ff9c9e
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-14 14:16:31 -0300

    Update login.js

--- credentials/login.js
+++ credentials/login.js
@@ -1,4 +1,4 @@
-const dbchannels = require("./config.js").channels
+const dbchannels = require("./config.js")
   
   
 const options = {
@@ -13,7 +13,7 @@ const options = {
     username: "LuuLBot",
     password: "oauth:4lcd5inaptc5tncqsumrugzxro2p6l",
   },
-  channels: dbchannels
+  channels: dbchannels.channels
 };
   
 module.exports = { options }
