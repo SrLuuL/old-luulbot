3c2308226066607722b975a060c5f48af888ca89
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-14 14:11:47 -0300

    Update config.js

--- credentials/config.js
+++ credentials/config.js
@@ -2,7 +2,8 @@ const db = require("quick.db")
 
 
 db.set("channels")
+db.push("channels.items", "#srluul")
 
-const channels = db.get("channels")
+const channels = db.get("channels.items")
 
 exports = {channels}
