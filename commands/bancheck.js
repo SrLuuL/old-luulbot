285bfacac93f9f433d10838b3c1cc99dcd98994e
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-17 16:14:57 -0300

    Create bancheck.js

---
+++ commands/bancheck.js
@@ -0,0 +1,46 @@
+module.exports.run = async (client, message, args, username, channel) => {
+
+const ms = require("pretty-ms")
+const fetch = require("node-fetch")
+
+let user = args[0];
+let user2 = args[1];
+
+if (!user) return client.say(channel, `${username}, insira um usuário :/`)
+
+if (!user2) {
+user = username
+user2 = args[0]
+}
+
+const {status, banned, isPermanent, createdAt, expiresAt} = fetch(`https://api.ivr.fi/twitch/banlookup/teste/srluul`).json();
+
+if (status === 500) return client.say(channel, `${username}, usuários inválidos :/`)
+
+let DateBan = new Date(createdAt)
+let DateExpire = new Date(expiresAt)
+DateBan = DateBan - Date.now();
+DateBan = ms(DateBan, {secondsDecimalDigits: 0});
+DateExpire = DateExpire - Date.now();
+DateExpire = ms(DateExpire, {secondsDecimalDigits: 0});
+
+if (banned === false) {
+return client.say(channel, `${username}, ${user} não está banido em ${user2}`)
+} else {
+if (isPermanent === true) {
+return client.say(channel, `${username}, ${user} está banido permanentemente em ${user2} há ${DateBan}`)
+} else {
+return client.say(channel, `${username}, {user} está com timeout em ${user2} há ${DateBan} e vai acabar em ${DateExpire}`)
+}
+
+}
+
+}
+
+module.exports.config = {
+name: "bancheck",
+aliases: ["banchk"],
+description: "Verifica se um usuário está banido em algum canal",
+usage: "bancheck [user] [user2]"
+}
+
