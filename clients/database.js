d36b8819113646085d0c4008013f851034c5f2b0
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-20 10:57:39 -0300

    Create database.js

---
+++ clients/database.js
@@ -0,0 +1,14 @@
+const {Database} = require("pg")
+const connectionURI = "postgres://waanqlbezgjabs:f4e8c028930a2b89dd87a73d2090c0c24e81e6ccf8ee75f8fa985b60658483d2@ec2-52-1-95-247.compute-1.amazonaws.com:5432/dd7rfjbn2rp2uo"
+
+const options = { connectionString: connectionURI }
+
+
+const db = new Database(options)
+
+db.connect()
+.then(console.log("Database conectada!");
+
+db.on("error, (err) => {console.log(err)});
+
+module.exports = {db}
