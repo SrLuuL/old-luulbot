c70685e34f462e13bc84a12f223173874784292e
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-20 13:29:36 -0300

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -21,14 +21,14 @@ module.exports.run = (client, message, args, username, channel) => {
    }
  } else { query = args.join(" ") }
 
-  
+if (args[0}) {  
 if (args[0].startsWith("-")) {
 if(!db.query(`SELECT ${args[0].slice(1)}Weather FROM userWeather`)) {
 return client.say(channel, `${username}, este usuário não setou seu local :/`)
 } else {
  query =  db.query(`SELECT ${args[0].slice(1)} FROM userWeather`)
 }
-
+}
 }
                                                                                                                                                  
                                                                                                                                                                                                               
