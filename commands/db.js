c2b1ba5427f82c62c2050b39ec92604a92951861
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-20 11:48:20 -0300

    Update db.js

--- commands/db.js
+++ commands/db.js
@@ -6,11 +6,11 @@ if (username !== "srluul") return;
 
 db.query(`${args.join(' ')}`, (err,result) => {
    if(err){
-      return obote.say(channel, `@${username}, (${err.message}) ocorreu algum erro :/`)
+      return client.say(channel, `@${username}, (${err.message}) ocorreu algum erro :/`)
     }
     
    console.log(result.rows[0] || result)
-    obote.say(channel, `${username}, olhe os logs para os resultados :) `)
+    client.say(channel, `${username}, olhe os logs para os resultados :) `)
   })
   
   }
