5c7253d6b1eff343b7ead05a744e3eb2e40cecc8
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-20 11:46:58 -0300

    Update database.js

--- clients/database.js
+++ clients/database.js
@@ -1,9 +1,9 @@
-const {Database} = require("pg")
+const { Client } = require("pg")
 const connectionURI = "postgres://waanqlbezgjabs:f4e8c028930a2b89dd87a73d2090c0c24e81e6ccf8ee75f8fa985b60658483d2@ec2-52-1-95-247.compute-1.amazonaws.com:5432/dd7rfjbn2rp2uo"
 
 
 
-const db = new Database({connectionString: connectionURI})
+const db = new Client({connectionString: connectionURI})
 
 db.connect()
 
