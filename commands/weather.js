f72b9da16b80acbd029bb4b2c40cf76a62d01762
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-20 08:01:10 -0300

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -68,6 +68,7 @@ module.exports.run = (client, message, args, username, channel) => {
   
   if(args[0] == "--set" && args[1]) {
     db.set(`${username}_weather`, `args.join(" ").slice(args[0].length)`)
+    return client.say(channel, `${username}, local setado!`)
   }
   
   client.say(channel,`${username}, ${name}(${country}) ${clima} ${main}, ${temp}° com sensação de ${feel}°, ${humidity}% de humidade e ventos a ${wind} m/s \u{1F343}`)
