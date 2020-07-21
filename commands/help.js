983beaa5019181977b8d863fb12b2786d5f87c70
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-21 20:53:44 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports.run = (client, message, args, username, channel, cmd, alias) => {
  let donoCmds = ["db", "eval"]
  
 if (!args[0]) {
- let commands = cmd.map(c => `${c.config.name}`).filter(i => i.includes(donoCmds)).join(", ");
+ let commands = cmd.map(c => `${c.config.name.filter(i => !i.includes(donoCmds))}`).join(", ");
 
   
 client.say(channel, `${username}, Prefixo: = | Comandos: ${commands} | `)
