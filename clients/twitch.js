cc1dac299f0c6f8f89da9c70c4d18bd4ac94a16a
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-21 19:47:46 -0300

    Update twitch.js

--- clients/twitch.js
+++ clients/twitch.js
@@ -1,5 +1,13 @@
+const channels = require("../credentials/login.js).channelOptions
+
 const login = require("../credentials/login.js").options
 const tmi = require("tmi.js")
 const client = new tmi.client(login)
+
+client.connect()
+
+client.on("connected", async () => {
+  console.log(`Conectado nos canais: ${channels.join(" ")}`)
+});
 
 module.exports = { client }
