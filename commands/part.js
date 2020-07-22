7d8257a00d717639e0a2ad3b7f4305a6ff9ed9be
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-21 22:32:58 -0300

    Update part.js

--- commands/part.js
+++ commands/part.js
@@ -4,12 +4,14 @@ const db = require("../clients/database.js").db
 
 if (username !== "srluul") return;
 if (!args[0]) return client.say(channel, `${username}, nenhum canal no input :Z`)
-
+if (!channelsOptions.includes(args[0]) return client.say(channel, `${username}, não estou nesse canal :/`)
 
 
 db.query(`DELETE FROM luulbot_channels WHERE userchannel='${args[0]}' `, (err) => {if (err) throw err});
+let index = channelsOptions.indexOf(args[0])
+channelsOptions.splice(index, 1)  
 client.part(args[0]);
-channelsOptions.pop(`'${args[0]}'`)
+channelsOptions.delete(`${args[0]}`)
 client.say(channel, `${username}, saí de ${args[0]} com sucesso!`);
 
 
