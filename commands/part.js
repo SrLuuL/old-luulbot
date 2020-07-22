6c795c0bad86ef3f0eee55487e3c9e1645299722
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-21 21:56:16 -0300

    Update part.js

--- commands/part.js
+++ commands/part.js
@@ -8,12 +8,13 @@ if (!args[0]) return client.say(channel, `${username}, nenhum canal no input :Z`
 try{
 db.query(`DELETE FROM luulbot_channels WHERE userchannel='${args[0]}' `);
 client.join(args[0]);
-
+client.say(channel, `${username}, saí de ${args[0]} com sucesso!`);
 } catch (err) {client.say(channel, `${username}, não possuo este canal em minha database :/`)}
 
 }
 
 module.exports.config = {
+aliases: ["part"],
 description: "Conecta o bot em um canal",
 usage: "join [user]"
 }
