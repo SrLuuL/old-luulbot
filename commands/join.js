7542fdfa3e922258ec5ba0da8fb35b603d02f784
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-07-21 21:55:13 -0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -6,10 +6,11 @@ if (!args[0]) return client.say(channel, `${username}, nenhum canal no input :Z`
 
 db.query(`INSERT INTO luulbot_channels(userchannel) VALUES('${args[0]}')`);
 client.join(args[0]);
-
+client.say(channel, `${username}, entrei em ${args[0]} com sucesso!`)
 }
 
 module.exports.config = {
+aliases: ["join"],
 description: "Conecta o bot em um canal",
 usage: "join [user]"
 }
