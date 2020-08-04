d712dc2374307dbb2df8eeaee8aeda3f483333aa
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-03 21:43:52 -0300

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -1,13 +1,29 @@
 module.exports.run = async (client, message, args, username, channel) => {
 
 const fetch = require("node-fetch");
-
-const res = await (await fetch("https://mcapi.xdefcon.com/server/0.tcp.sa.ngrok.io:13104/full/json")).json();
-const {serverStatus, serverip, version, players, maxplayers, ping} = res;
+const haste = require("hastebin-gen");
+  
+const resSVInf = await (await fetch("https://mcapi.xdefcon.com/server/0.tcp.sa.ngrok.io:13104/full/json")).json();
+const resSVP = await (await fetch("https://api.mcsrvstat.us/2/0.tcp.sa.ngrok.io:13104")).json();
+const {serverStatus, serverip, version, players, maxplayers, ping} = resSVInf;
+const {list} = resSVP.players.list;
+const playersList = await haste(list);
 let format = `Servidor online! Players: ${players}/${maxplayers} | Ip: ${serverip} | Versão: ${version} | Ping: ${ping} ms`;
 
+  
+  switch(args[0]) {
+    case "players":
+      if (serverStatus === "offline") return client.say(channel, `${username}, servidor offline FeelsBadMan`)
+      else {
+        return client.say(channel, `${username}, Jogadores ativos: ${playersList}`)
+      }
+  }
+  
+  
 if (serverStatus === "offline") return client.say(channel, `${username}, servidor offline FeelsBadMan`)
-else { client.say(channel, `${username}, ${format}`)}
+else { 
+  client.say(channel, `${username}, ${format}`)
+}
 
 }
 
