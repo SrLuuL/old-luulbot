737f85ada265f0fe228b49059b5e809eb27e2e73
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-08 21:39:34 -0300

    Update amem.js

--- commands/amem.js
+++ commands/amem.js
@@ -1,18 +1,16 @@
 module.exports.run = async (client, message, args, username, channel) => {
 
 	
-const unirest = require("unirest")	
-const res = unirest("GET", "https://uncovered-treasure-v1.p.rapidapi.com/random");
+const fetch = require("node-fetch");
+const res = await (await fetch("https://uncovered-treasure-v1.p.rapidapi.com/random", { headers:{
+"x-rapidapi-host": "uncovered-treasure-v1.p.rapidapi.com",
+	"x-rapidapi-key": "0c01d9eb32msh1fed7589a51207dp1e2df4jsne0a034176750",
+	"useQueryString": true
+})).json
 const translate = require("@vitalets/google-translate-api");
 
 		
-res.headers({
-	"x-rapidapi-host": "uncovered-treasure-v1.p.rapidapi.com",
-	"x-rapidapi-key": "0c01d9eb32msh1fed7589a51207dp1e2df4jsne0a034176750",
-	"useQueryString": true
-});
-
-const {context, text} = await res.body.results[0];
+const {context, text} = res.results[0];
 const translated = await translate(text, {to:"pt"});
 	
 client.say(channel, `${username}, (${context}) ${translated}`)
