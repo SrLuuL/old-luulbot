60e7b79edfbea33caa9f5f7fe4677ed20382bcee
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-08 21:40:20 -0300

    Update amem.js

--- commands/amem.js
+++ commands/amem.js
@@ -2,11 +2,11 @@ module.exports.run = async (client, message, args, username, channel) => {
 
 	
 const fetch = require("node-fetch");
-const res = await (await fetch("https://uncovered-treasure-v1.p.rapidapi.com/random", { headers:{
+const res = await (await fetch("https://uncovered-treasure-v1.p.rapidapi.com/random", {headers:{
 "x-rapidapi-host": "uncovered-treasure-v1.p.rapidapi.com",
 	"x-rapidapi-key": "0c01d9eb32msh1fed7589a51207dp1e2df4jsne0a034176750",
 	"useQueryString": true
-})).json
+}})).json
 const translate = require("@vitalets/google-translate-api");
 
 		
