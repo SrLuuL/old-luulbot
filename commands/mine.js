2e327f71e5706284ea684ee005401246592299a7
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-09 17:23:20 -0300

    Create mine.js

---
+++ commands/mine.js
@@ -0,0 +1,20 @@
+module.exports.run = async (client, message, args, username, channel) => {
+
+const fetch = require("node-fetch");
+
+const ip = "kekwdance.pvp.host";
+const res = await (await fetch(`https://api.mcsrvstat.us/2/${ip}`)).json();
+const {online, max} = res.players;
+const serverOnline = res.online;
+const format = `Servidor Online! Ip:${ip} | Players: ${online}/${max} | Mods: https://bit.ly/3fMCWzv`
+
+if (serverOnline === true) ? client.say(channel, `${username}, ${format}`) : client.say(channel, `${username}, servidor offline FeelsBadMan`);
+
+}
+
+module.exports.config = {
+name: "mine",
+aliases: ["mn","sv"],
+description: "Informações sobre o servidor de mine",
+usage: "mine"
+}
