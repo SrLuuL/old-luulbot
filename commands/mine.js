ef191fbaaa6b923feab03e28184aa0391119a3b1
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-09 18:18:43 -0300

    Update mine.js

--- commands/mine.js
+++ commands/mine.js
@@ -4,12 +4,12 @@ const fetch = require("node-fetch");
 
 const ip = "kekwdance.pvp.host";
 const res = await (await fetch(`https://api.mcsrvstat.us/2/${ip}`)).json();
-const {online, max} = res.players;
 const serverOnline = res.online;
+if (serverOnline === false) return client.say(channel, `${username}, servidor offline FeelsBadMan`);
+const {online, max} = res.players;
 const format = `Servidor Online! Ip: ${ip} | Players: ${online}/${max} | Mods: https://bit.ly/3fMCWzv`
 
-if (serverOnline === true) { client.say(channel, `${username}, ${format}`) } 
-else { client.say(channel, `${username}, servidor offline FeelsBadMan`); }
+client.say(channel, `${username}, ${format}`);
 
 }
 
