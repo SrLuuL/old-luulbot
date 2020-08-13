d9f8224ec7810af4225f7ec28cc11d910333881f
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-12 23:45:58 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 const client = require ("./clients/twitch.js").client
 const luulbot = require ("./clients/discord.js").luulbot
-
-
+const triviaInfo = require("./commands/trivia.js").triviaInfo
+const triviaAnswer = require("./commands/trivia.js").answer
 
 
 
@@ -16,7 +16,7 @@ client.on("message", async (channel, user, message, self) => {
 	let username = user.username
 	let args = message.slice(prefix.length).trim().split(/ +/g);
 	let command = args.shift().toLowerCase();
-	
+	let canal = channel.replace("#", "");
 
 	
 	
@@ -32,7 +32,15 @@ let cmdfile = luulbot.commands.get(command) || luulbot.commands.get(luulbot.alia
 
 if (cmdfile) cmdfile.run(client, message, args, username, channel, cmd, alias);
 	
-
+	
+if (triviaInfo.find(i => i.channel)) {
+	if (message.toLowerCase().includes(triviaAnswer)) {
+		let triviaIndex = triviaInfo.find(i => channel == canal);
+		triviaInfo.splice(triviaIndex, 1);
+		client.say(channel, `${username} acertou a pergunta :O`);
+	}
+}
+	
 setTimeout(() => {
 	globalCD.delete(username)
 }, 5000);
