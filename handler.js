960e5be659c5b1f0fa357c39f0b04e0f183e9c85
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-13 00:22:21 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -62,8 +62,8 @@ client.say(channel, `:/ A resposta era: ${answer}`);
 	
 	
 if (triviaInfo.find(i => i.channel === canal)) {
-	let triviaAnswer = triviaInfo.find(i => i.channel === canal).answer
-	if (message.toLowerCase().includes(triviaAnswer)) {
+	let triviaAnswer = triviaInfo.answer
+	if (triviaAnswer.includes(message.toLowerCase())) {
 		let triviaIndex = triviaInfo.find(i => channel === canal);
 		triviaInfo.splice(triviaIndex, 1);
 		client.say(channel, `${username} acertou a pergunta :O`);
