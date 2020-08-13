de591974acb700da87bc2fb4882c15a5daa3829d
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-13 17:29:40 -0300

    Update trivia.js

--- data/trivia.js
+++ data/trivia.js
@@ -3,5 +3,10 @@
 "category": "Animais",
 "question": "O Axolote é um anfíbio que consegue viver sua vida inteira no estado larval",
 "answer": "Verdade"
-}
+},
+  {
+   "category": "Ciências e Natureza",
+   "question": "Rinoplastia é uma cirurgia que é efetuada em que parte do corpo?",
+  "answer": "Nariz"
+  }
 ]
