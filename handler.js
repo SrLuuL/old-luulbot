0a345e505844c706635657277c015114076cc942
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-16 17:15:43 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 const client = require ("./clients/twitch.js").client
 const luulbot = require ("./clients/discord.js").luulbot
 const fetch = require("node-fetch");
-
+const trivia = [];
 
 let prefix = "=";
 let globalCD = new Set();
@@ -16,14 +16,21 @@ client.on("message", async (channel, user, message, self) => {
 	let args = message.slice(prefix.length).trim().split(/ +/g);
 	let command = args.shift().toLowerCase();
 	let canal = channel.replace("#", "");
+	
+	if (trivia.find(i => i.channel === channel)) {
+		let answer = trivia.find(i => i.channel === channel).answer;
+		if (answer.includes(message.toLowerCase())) {
+			client.say(channel, `${username} acertou a pergunta! (${answer})`)
+			let triviaIndex = trivia.findIndex(i => i.channel === channel);
+			trivia.splice(triviaIndex, 1);
+		}
+	}
+	
 
 	if (self) return;
 	
 	if (globalCD.has(username)) return;
-	
-	
-	
-	
+
 	if (!message.startsWith(prefix)) return;
 
 globalCD.add(username);
@@ -38,8 +45,38 @@ setTimeout(() => {
 }, 5000);
 
 	
-
+if (message.startsWith(prefix + "trivia")) {
 	
+	if (username !== "srluul") return;
+	if (trivia.find(i => i.running)) return;
+	
+	startTrivia()
+	
+	function startTrivia() {
+
+		const quiz = require("./data/trivia.json")
+		const items = quiz[Math.floor(Math.random() * quiz.length)];
+		
+		const question = items.question
+		const category = items.category
+		const answer = items.category
+		
+		client.say(channel, `Categoria: ${category} | ${answer}`)
+		trivia.push({ channel: channel, running: true, answer: answer });
+		checkTrivia()
+	}
+	
+	
+	function checkTrivia() {
+		if (trivia.find(i => i.channel === channel)) {
+			setTimeout(async() => {
+				let answer = trivia.find(i => i.channel === channel).answer[0];
+				client.say(channel, `Ninguém acertou :/, a resposta era: ${answer}`);
+				let triviaIndex = trivia.findIndex(i => i.channel === channel);
+				trivia.splice(triviaIndex, 1)
+			}, 35000)
+		}
+	}
 });
 
 
