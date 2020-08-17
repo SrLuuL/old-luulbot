089111869870915402c20e3445f0f2a1c4f11b70
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-17 00:45:42 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -38,58 +38,6 @@ setTimeout(() => {
 }, 5000);
 
 	
-if (message.startsWith(prefix + "trivia")) {
-	
-	if (username !== "srluul") return;
-	if (trivia.find(i => i.running)) return;
-	
-	startTrivia()
-	
-	async function startTrivia() {
-
-		const quiz = require("./data/trivia.json")
-		const items = quiz[Math.floor(Math.random() * quiz.length)];
-		
-		const question = items.question
-		const category = items.category
-		const answer = items.answer
-		
-		client.say(channel, `Categoria: ${category} | ${question}`)
-		trivia.push({ channel: channel, running: true, answer: answer });
-		
-		const done = new Promise(resolve => {
-			const timer = setTimeout(() => {
-				client.say(channel, `Ninguém acertou :/, a resposta era ${answer[0]}`)
-				let triviaIndex = trivia.findIndex(i => i.channel === channel);
-				trivia.splice(triviaIndex, 1)
-				resolve()
-			}, 35000)
-			
-			
-			client.on("chat", async (channel, user, message, self) => {
-			
-			const similarity = compareStrings(message, answer[0])
-			
-			if (similarity < 0.9) return;
-			
-			clearTimeout(timer)
-			client.say(channel, `${username} acertou!, a resposta era ${answer[0]}`)
-			let triviaIndex = trivia.findIndex(i => i.channel === channel);
-			trivia.splice(triviaIndex, 1)
-			resolve()
-			
-			})
-			
-		
-			
-		})
-		
-		await done
-			 
-	}
-
-	
-}
 	
 });
 
