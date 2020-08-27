4c304c13e8734c74995fd25f5ecf98ed30c9a2a6
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-26 22:44:37 -0300

    Update trivia.js

--- commands/trivia.js
+++ commands/trivia.js
@@ -26,10 +26,8 @@ if (!trivia.running && args[0] == "start") {
 
 if(args[1]) {
 if (/^-?\d+$/.test(args[1]) === false) { num = 1 };   
-if (args[1] > 100 || args[1] < 1) return client.say(channel, `${username} número muito grande/baixo :/`)
-else {
-  num = args[1]
-}
+if (args[1] > 100 || args[1] < 1) { num = 100 }
+else { num = args[1] }
 }
   
 trivia.running = true
