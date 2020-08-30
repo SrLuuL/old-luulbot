c6994988a7b0dbd2ffff5efa266f39ef46cb1d2e
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-29 23:08:58 -0300

    Update emoteinfo.js

--- commands/emoteinfo.js
+++ commands/emoteinfo.js
@@ -1,23 +1,20 @@
 module.exports.run = async (client, message, args, username, channel) => {
 
-const fetch = require("node-fetch")
+const fetch = require("node-fetch");
 
-let res = await fetch(`https://api.ivr.fi/twitch/emotes/${args[0]}`);
-let data = await res.json();
-
-let user = data.channel
-const {emoteid, emotecode, tier} = data;
+const res = await (await fetch(`https://api.ivr.fi/twitch/emotes/${args[0]}`)).json();
+  
+const {channel:channelEmote, emoteid, emotecode, tier} = res;
 
 let url = `https://twitchemotes.com/emotes/${emoteid}`  
   
-if (data.status === 404 || data.status === 500) {
-  return client.say(channel, `${username}, não encontrei esse emote :/`)
-}
+if (data.status === 404 || data.status === 500) return client.say(channel, `${username}, não encontrei esse emote :/`);
+
 
 if (!args[0]) {
-return client.say(channel, `${username}, insira um emote :/`)
+client.say(channel, `${username}, insira um emote :/`)
 } else {
-client.say(channel, `${username}, Emote: ${emotecode} | ID: ${emoteid} | Canal: ${user} | Tier: ${tier} | ${url}`)
+client.say(channel, `${username}, Emote: ${emotecode} | ID: ${emoteid} | Canal: ${channelEmote} | Tier: ${tier} | ${url}`)
 }
 
 }
