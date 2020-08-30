fa677cbeed8dc8d903ddff699be1a5676da42ce4
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-30 14:34:34 -0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,7 @@
 module.exports.run = async (client, message, args, username, channel) => {
 
-  const db = require("../clients/database.js").db
+  const db = require('../clients/database.js').db
+  const ms = require('pretty-ms');
   
   if (username !== "srluul") return;
 
@@ -8,12 +9,13 @@ module.exports.run = async (client, message, args, username, channel) => {
   
 let content = args.join(" ")
 
+const startTime = Date.now()
 const evaluated = await eval('(async () => {' +args.join(" ") + '})()')
 const ev = String(evaluated)
-
+const endTime = Date.now()
 
 if (!args[0]) return client.say(channel, `${username}, insira um código :/`)
-else client.say(channel, `${ev}(feito em ${endTime - startTime} ms`)
+else client.say(channel, `${ev}(feito em ${ms(endTime - startTime)} ms`)
 
     
 
