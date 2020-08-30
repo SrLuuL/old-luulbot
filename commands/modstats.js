99397143fbde91266b4d5d4ee30f0291c33b7b29
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-08-29 23:20:11 -0300

    Update modstats.js

--- commands/modstats.js
+++ commands/modstats.js
@@ -2,43 +2,24 @@ module.exports.run = async (client, message, args, username, channel) => {
 
 const fetch = require("node-fetch");
 const hastebin = require("hastebin-gen");
-let userid;
 
-if (!args[0]) {
-userid = username
-} else {
-userid = args[0]
-}
-
-let req = await fetch(`https://modlookup.3v.fi/api/user-v3/${userid}`)
-let req2 = await fetch(`https://modlookup.3v.fi/api/user-totals/${userid}`)
-
-let data = await req.json()
-let channels = data.channels
-let num = Object.keys(channels).length
-let list = [];
-for(let x = 0; x < num; x++) {
-list.push(channels[x].name)
-}
+const userid = (!args[0]) ? username : args[0];
   
-
+const res = await (await fetch(`https://modlookup.3v.fi/api/user-v3/${userid}`)).json();
+const res2 = await (await fetch(`https://modlookup.3v.fi/api/user-totals/${userid}`)).json();  
+  
+const modsList = res.channels.map(i => i.name)  
+ 
  try { 
-const haste = await hastebin(list)  
+   
+const haste = await hastebin(modsList)  
 
+const {user, views, follows, total, partners} = res2;
 
+const sender = (user === username) ? 'você' : user;   
 
-let data2 = await req2.json()
-
-const {user, views, follows, total, partners} = data2;
-
-
-if (!args[0]) {
-
-return client.say(channel, `${username}, você é mod em ${total} canais, ${partners} sendo parceiros | Views totais: ${views} | Follows totais: ${follows} | Canais: ${haste} `)
-
-} else {
-return client.say(channel, `${username}, ${user} é mod em ${total} canais, ${partners} sendo parceiros | Views totais: ${views} | Follows totais: ${follows} | Canais: ${haste} `)
-}
+return client.say(channel, `${username}, ${user} é mod em ${total} canais, ${partners} sendo parceiros | Views totais: ${views} | Follows totais: ${follows} | Canais: ${haste}`)
+   
 
  } catch (err) {
    client.say(channel, `${username}, serviço indisponível :/`)
