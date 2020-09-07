af9e10246bdff0292cf87959bf2ec3c00bfb2a4b
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-07 18:38:39 -0300

    Update functions.js

--- events/functions.js
+++ events/functions.js
@@ -7,6 +7,6 @@ const resFFZ = await (await fetch(`https://decapi.me/ffz/emotes/${channel}`)).te
 const resBTTV = await (await fetch(`https://decapi.me/bttv/emotes/${channel}`)).text();
 const resTwitch = await (await fetch(`https://decapi.me/twitch/subscriber_emotes/${channel}`)).text();  
 
-emotes.push({ffz: resFFZ, bttv: resBTTV, twitch: resTwitch})  
+await emotes.push({ffz: resFFZ, bttv: resBTTV, twitch: resTwitch})  
 return emotes  
 }
