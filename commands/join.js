e315bdc9d476b1c7e1a8287660b55d10124dba58
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-09 13:26:35 -0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -6,7 +6,7 @@ if (username !== "srluul") return;
 if (!args[0]) return client.say(channel, `${username}, nenhum canal no input :Z`)
 const getId = await db.query('select userid from luulbot_channels')  
 
-db.query(`INSERT INTO luulbot_channels(userchannel, userid) VALUES('${args[0]}', '${getId.rows.pop().userid + 1}')`);
+db.query(`INSERT INTO luulbot_channels(userchannel, userid) VALUES('${args[0]}', '${getId.rows.length + 1}')`);
 client.join(args[0]);
 channelsOptions.push(`${args[0]}`)
 client.say(channel, `${username}, entrei no canal ${args[0]} com sucesso!`)
