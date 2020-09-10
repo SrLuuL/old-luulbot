142a4fda5ce28d8eebc2d685466c3a48f80ea5bf
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-09 23:19:05 -0300

    Update sugerir.js

--- commands/sugerir.js
+++ commands/sugerir.js
@@ -2,12 +2,12 @@ module.exports.run = async (client, message, args, username, channel) => {
 
   const db = require("../clients/database.js").db
   
-  let total = await db.query(`SELECT userchannel FROM luulbot_suggests`)
+  let total = await db.query(`SELECT suggestid FROM luulbot_suggests ORDER BY suggestid ASC `)
   
   if (!args[0]) return client.say(channel, `${username}, insira uma sugestão :/`)
   
-  await db.query(`INSERT INTO luulbot_suggests(userchannel, usersuggest, suggestid) VALUES('${username}','${args.join(" ")}', '${total.rows.length + 1}')`)
-  client.say(channel, `${username}, sugestão anotada :D 📝 (ID:${total.rows.length + 1})`)
+  await db.query(`INSERT INTO luulbot_suggests(userchannel, usersuggest, suggestid) VALUES('${username}','${args.join(" ")}', '${total.rows.pop().suggestid + 1}')`)
+  client.say(channel, `${username}, sugestão anotada :D 📝 (ID:${total.rows.pop().suggestid + 1})`)
 
 }
 
