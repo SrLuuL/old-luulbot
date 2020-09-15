d1227e9e475de8b1859eac940f40a36313e04198
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-15 14:20:40 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -10,7 +10,7 @@ let alias = luulbot.aliases;
 
 client.on('connected', async () => {
 	await db.query(` UPDATE luulbot_info SET value = ${Date.now()} WHERE setting = 'uptime' `)
-	await db.query(` UPDATE luulbot_info SET value = 0 WHERE setting = 'command_value' `)
+	await db.query(` UPDATE luulbot_info SET value = 0 WHERE setting = 'command_count' `)
 })
 
 client.on('message', async (channel, user, message, self) => {
@@ -31,7 +31,7 @@ client.on('message', async (channel, user, message, self) => {
 let cmdfile = luulbot.commands.get(command) || luulbot.commands.get(luulbot.aliases.get(command))
 
 if (cmdfile) {
-	await db.query(` UPDATE luulbot_info SET value = value + 1 WHERE setting = 'command_value' `)
+	await db.query(` UPDATE luulbot_info SET value = value + 1 WHERE setting = 'command_count' `)
 	cmdfile.run(client, message, args, username, channel, cmd, alias);
 	globalCD.add(username);
 }
