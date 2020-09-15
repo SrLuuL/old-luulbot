99b83c7af5b9dbca73e647e8f25887f0a86672d1
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-15 00:42:49 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,11 +3,10 @@ module.exports.run = (client, message, args, username, channel, cmd, alias) => {
  let donoCmds = ['db', 'eval', 'join', 'part']
  
 if (!args[0]) {
- let commands = cmd.map(c => `${c.config.name}`).join(", ");
- const commandsFixed = commands.split(' ').filter(index => !index.includes(donoCmds));
+ let commands = cmd.map(c => `${c.config.name}`).filter(index => !index.includes(donoCmds));
 
   
-client.say(channel, `${username}, Prefixo: = | Comandos: ${commandsFixed.join('/')} | `)
+client.say(channel, `${username}, Prefixo: = | Comandos: ${commands.join('/')} | `)
 } else {
 let command = cmd.get(alias.get(args[0].toLowerCase()) || args[0].toLowerCase());
 if (!command) return client.say(channel, `${username}, comando inexistente  :/ `)
