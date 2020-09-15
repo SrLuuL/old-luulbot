d4f684eb89399fb29a0becb31fbf254b4dfbe518
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-15 14:19:01 -0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,15 +5,22 @@ module.exports.run =  async (client, message, args, username, channel) => {
 	const db = require('../clients/database.js').db;
         const si = require("systeminformation");
 	const ms = require('pretty-ms');
+	
+	
         let total = process.memoryUsage().heapTotal / 1024 / 1024;
 	let realtotal = Math.round(total * 100) / 100;
+	
 	const ping = await client.ping() * 1000;
-	const date = await db.query(`SELECT * FROM luulbot_info WHERE setting = 'uptime'`)
+	
+	const date = await db.query(` SELECT * FROM luulbot_info WHERE setting = 'uptime' `)
 	const uptime = await ms(Date.now() - date.rows[0].value, {secondsDecimalDigits: 0, unitCount: 3});
+	
+	const commandTotal = await db.query(` SELECT * FROM luulbot_info WHERE setting = 'command_value' `);
+	const commandValue = commandTotal.rows[0].value;
 
 	
 	
-client.say(channel, `Pong! Ping: ${ping} ms | Memória: ${realtotal} MB | Uptime: ${uptime}`)
+client.say(channel, ` Pong! Ping: ${ping} ms | Memória: ${realtotal} MB | Uptime: ${uptime} | Comandos usados: ${commandValue} `)
 	
 }
 
