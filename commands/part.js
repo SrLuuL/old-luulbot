c859ce829c4cf0a5fb263936a4777b36edc05b8f
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-16 18:49:42 -0300

    Update part.js

--- commands/part.js
+++ commands/part.js
@@ -1,17 +1,17 @@
 module.exports.run = async (client, message, args, username, channel) => {
-const channelsOptions = require("../credentials/login.js").channelsOptions
+const channels = require("../credentials/login.js").channelOptions;
 const db = require("../clients/database.js").db
 
 if (username !== "srluul") return;
 if (!args[0]) return client.say(channel, `${username}, nenhum canal no input :Z`)
-if (!channelsOptions.includes(args[0])) return client.say(channel, `${username}, não estou nesse canal :/`)
+if (!channels.includes(args[0])) return client.say(channel, `${username}, não estou nesse canal :/`)
 
 
 await db.query(`DELETE FROM luulbot_channels WHERE userchannel='${args[0]}'`)
-let index = channelsOptions.indexOf(args[0])
-channelsOptions.splice(index, 1)  
+let index = channels.indexOf(args[0])
+channels.splice(index, 1)  
 client.part(args[0]);
-channelsOptions.delete(`${args[0]}`)
+channels.delete(`${args[0]}`)
 client.say(channel, `${username}, saí do canal ${args[0]} com sucesso!`);
 
 
