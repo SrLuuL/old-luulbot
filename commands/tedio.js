8285bc16550a0f92f6489172284baf4266383b06
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-22 11:11:11 -0300

    Update tedio.js

--- commands/tedio.js
+++ commands/tedio.js
@@ -1,13 +1,14 @@
 module.exports.run = async (client, message, args, username, channel) => {
 
 const fetch = require("node-fetch");
-const translate = require("@vitalets/google-translate-api");
+const translateapi = require("@kaysilvn/google-translate-api");
+const translate = new translateapi().translate;
 
 const res = await (await fetch("http://www.boredapi.com/api/activity/")).json();
 const {activity} = res;
-const res2 = await translate(activity, {to:"pt"});
+const res2 = await translate(activity, { src_lang: 'auto', tar_lang:'pt'});
 
-client.say(channel, `${username}, 🤔 ${res2.text}`)
+client.say(channel, `${username}, 🤔 ${res2}`)
 
 }
 
