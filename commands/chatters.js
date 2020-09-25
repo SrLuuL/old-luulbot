223b7773c933c59eb3e01765124637da3efaaa21
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-25 18:34:41 -0300

    Update chatters.js

--- commands/chatters.js
+++ commands/chatters.js
@@ -2,30 +2,24 @@ module.exports.run = async (client, message, args ,username, channel) => {
 
 const fetch = require("node-fetch")
 
-let user;
-user = (!args[0]) ? user = channel.slice(1) : user = args[0].toLowerCase();
+let user = (!args[0]) ?  channel.slice(1) :  args[0].toLowerCase();
 
 const res = await (await fetch(`https://tmi.twitch.tv/group/user/${user}/chatters`)).json();  
+const {chatter_count} = res;  
+
+if (!chatter_count) return client.say(channel, `${username}, usuário não possui chatters :/`)
+
+const {chatters: {vips, moderators, staff}} = res;
+  
+const verifier = [(vips ? vips.length + ' vips' : 0), (moderators ? moderators.length + ' mods' : 0), (staff ? staff.length + ' staffs' : 0)];
+const fullFormat = verifier.filter(Boolean).join('/');
   
 
-if (!res.chatter_count) return client.say(channel, `${username}, usuário não possui chatters :/`)
-
-const {chatter_count} = res;
-
-const getRoles = ({vips, moderators, staff}) => {
-  const v = (vips.length > 0) ? `${vips.length} vips` : 0
-  const m = (moderators.length > 0) ? `${moderators.length} mods` : 0
-  const s = (staff.length > 0) ? `${staff.length} staffs` : 0
-  
-  if (![v,m,s].find(i => i)) return ''
-  
-  return `(${[v,m,s].filter(Boolean).join('/')})`
-} 
   
 user = (user === username) ? "Você" : user;  
 
   
-client.say(channel, `${username}, ${user} possui ${chatter_count} chatters ${getRoles(res.chatters)} presentes neste momento`)
+client.say(channel, `${username}, ${user} possui ${chatter_count} chatters ${fullFormat} presentes neste momento`)
 
 }
 
