ae58ffcf0469c05c966f36d6b8e6c42ea5b3e9fd
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-09-25 23:24:04 -0300

    Update functions.js

--- events/functions.js
+++ events/functions.js
@@ -1,28 +1,24 @@
 const client = require('../clients/twitch.js').client;
 const fetch = require('node-fetch');
 
-(async () => {
 
-await client.getEmotes = async (channel) => {
+ client.getEmotes = async (channel) => {
   
 const emotes = [];
+   
 const urls = {
   ffzEmotes: `https://decapi.me/ffz/emotes/${channel}`,
   bttvEmotes: `https://decapi.me/bttv/emotes/${channel}`,
   twitchEmotes: `https://decapi.me/twitch/subscriber_emotes/${channel}`
 };
   
-
-  
   for(const index in urls) {
-    
     const res = await (await fetch(urls[index])).text()
     emotes.push({ [index]: res })
-    
   }
   
   return emotes;
   
 }
 
-});
+
