344ed9473ff89878078f3604a7244bf03d0f4021
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-10 17:20:28 -0300

    Update chatters.js

--- commands/chatters.js
+++ commands/chatters.js
@@ -11,7 +11,11 @@ if (!chatter_count) return client.say(channel, `${username}, usuário não possu
 
 const {chatters: {vips, moderators, staff}} = res;
   
-const verifier = [(vips.length > 0 ? vips.length + ' vips' : 0), (moderators.length > 0 ? moderators.length + ' mods' : 0), (staff.length > 0 ? staff.length + ' staffs' : 0)];
+let modsTotal = (moderators.length > 1) ? `${moderators.length} mods` : `1 mod`;
+let vipsTotal = (vips.length > 1) ? `${vips.length} vips` : `1 vip`;
+let staffsTotal = (staff.length > 1) ? `${staff.length} staffs` : `1 staff`;  
+  
+const verifier = [(vips.length > 0 ? vipsTotal : 0), (moderators.length > 0 ? modsTotal : 0), (staff.length > 0 ? staffsTotal : 0)];
 let fullFormat = verifier.filter(Boolean)
   
 if (!fullFormat.find(index => index)) {
