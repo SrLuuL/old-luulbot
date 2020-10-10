cd02e1f2df3548c2de49d62eb8c7ceac72992f37
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-10 18:55:33 -0300

    Update modstats.js

--- commands/modstats.js
+++ commands/modstats.js
@@ -16,9 +16,12 @@ const haste = await hastebin(modsList)
 
 const {user, views, follows, total, partners} = res2;
 
-const sender = (user === username) ? 'você' : user;   
+const sender = (user === username) ? 'você' : user; 
+   
+let channelTotal = total ? total > 1 ? `${total} canais` : `1 canal` : 'nenhum canal';
+let partnerTotal = partners ? partners > 1 ? `(${partners}  parceiros)` : '(1 parceiro)' : '';     
 
-return client.say(channel, `${username}, ${sender} é mod em ${total} canais, ${partners} sendo parceiros | Views totais: ${views} | Follows totais: ${follows} | Canais: ${haste}`)
+return client.say(channel, `${username}, ${sender} é mod em ${channelTotal}(partnerTotal) | Views totais: ${views} | Follows totais: ${follows} | Canais: ${haste}`)
    
 
  } catch (err) {
