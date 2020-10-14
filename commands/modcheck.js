d5fa0afeaa772a213cf528e1e3ec8a98f33bdb3b
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-14 14:48:32 -0300

    Update modcheck.js

--- commands/modcheck.js
+++ commands/modcheck.js
@@ -4,34 +4,39 @@ const fetch = require('node-fetch');
 const ms = require('pretty-ms');
 
 let sender;
-
+let user;
 
 if (args[0] && args[1]) {
-sender = args[1];
+sender = args[1].toLowerCase();
+user = args[0].toLowerCase();  
 } else if (args[0] && !args[1]) {
-sender = args[0];
+sender = args[0].toLowerCase();
+user = username; 
 } else {
 sender = channe.replace('#', '');
+user = username;
 }
 
 
 const res = await (await fetch(`https://api.ivr.fi/twitch/modsvips/${sender}`)).json();
 
-
+const modCheck = res.mods.find(i => i.login === user)
+  
+  
 if (args[0] && args[1]) {
 
-if (res.mods.find(i => i.login === args[0].toLowerCase())) {
+if (modCheck) {
 
-let modTime = ms(Date.now() - new Date(res.mods.find(i => i.login === args[0].toLowerCase()).grantedAt), {secondsDecimalDigits: 0, unitCount: 3});
+let modTime = ms(Date.now() - new Date(modCheck.grantedAt), {secondsDecimalDigits: 0, unitCount: 3});
 
-let senderUser =  (args[0].toLowerCase() === username) ? 'você' : args[0];
-let senderChannel =  (args[1].toLowerCase() === username) ? 'seu canal' : args[1];  
+sender = (sender === username) ? 'seu canal' : user;
+user = (user === username) ? 'você' : user;
   
-return client.say(channel , `${username}, ${args[0]} é mod em ${args[1]} há ${modTime} atrás`)
+return client.say(channel , `${username}, ${user} é mod em ${sender} há ${modTime} atrás`)
 
 } else {
 
-return client.say(channel, `${username}, ${args[0]} não é mod em ${args[1]}`)
+return client.say(channel, `${username}, ${user} não é mod em ${sender}`)
   
 }
 
