882354084a1862268de33039b1018a20a065714c
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-23 15:53:09 -0300

    Update site.js

--- site.js
+++ site.js
@@ -27,6 +27,7 @@ res.send(`
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <title> LuuLBot - Comandos </title>
+<link rel='icon' href='https://cdn.frankerfacez.com/010a6a6829cfe953dbe1958557424bc4.png'>
 
 </head>
 
