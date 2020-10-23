f2edbc1bb80d18d2dc9b29d2fc1521ab3e8f285d
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-23 15:35:28 -0300

    Update site.js

--- site.js
+++ site.js
@@ -8,9 +8,22 @@ const luulbot = require('./clients/discord.js').luulbot;
 app.get('/comandos', (req, res) => {
   
   let commandList = luulbot.commands.map(i => `
+<table>
+<tr>
+<td>NOME</td>
+<td>ALIASES</td>
+<td>DESCRIÇÃO</td>
+<td>USO</td>
+<td>PERMISSÃO</td>
+</tr>
 <tr>
 <td><a>${i.config.name}</a></td>
+<td><a>${i.config.aliases || '(nenhum)'}</a></td>
+<td><a>${i.config.description}</a></td>
+<td><a>${i.config.usage}</a></td>
+<td><a>${i.config.level}</a></td>
 </tr>
+</table>
 `);
   
   
@@ -21,6 +34,7 @@ res.send(`
 
 <meta charset='utf-8'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <title> LuuLBot - Comandos </title>
 
 </head>
