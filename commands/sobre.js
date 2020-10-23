df6c168eb77eac2c66bfceb70e1edddd0cad9c1c
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-23 18:03:45 -0300

    Create sobre.js

---
+++ commands/sobre.js
@@ -0,0 +1,13 @@
+module.exports.run = (client, message, args, username, channel) => {
+
+client.say(channel, `${username}, Bot feito com Node.js, sendo hosteado atualmente na Heroku e conectado em ${client.getChannels().length} canais`)
+
+}
+
+module.exports.config = {
+name: 'sobre',
+aliases: ['bot','about'],
+usage: 'sobre'
+description: 'Conta detalhes sobre o bot',
+level: 'Todos'
+}
