850ba63186cbce096734eef6dd959205a4af2c6f
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-23 19:08:14 -0300

    Update site.js

--- site.js
+++ site.js
@@ -92,6 +92,20 @@ background-color: #222b36;
 
 </style>
 
+<nav class='navbar navbar-expand navbar-dark bg-dark'>
+<a class='navbar-brand'>LuuLBot</a>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="/comandos">Comandos</a>
+</li>
+</ul>
+</div>
+</nav>
+
+
+
+</body>
 
 </html>
 
