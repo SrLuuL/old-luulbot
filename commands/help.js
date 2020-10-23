b5134e65855fe2038a5875c9aae47b1f5f21735f
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-23 16:30:38 -0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,10 +3,10 @@ module.exports.run = (client, message, args, username, channel, cmd, alias) => {
  let donoCmds = ['db', 'eval', 'join', 'part']
  
 if (!args[0]) {
- let commands = cmd.map(c => `${c.config.name}`).filter((index) => !donoCmds.includes(index));
+
 
   
-client.say(channel, `${username}, Prefixo: = | Comandos: ${commands.join(' / ')} | `)
+client.say(channel, `${username}, comandos em: http://luulbot.herokuapp.com/comandos`)
 } else {
 let command = cmd.get(alias.get(args[0].toLowerCase()) || args[0].toLowerCase());
 if (!command) return client.say(channel, `${username}, comando inexistente  :/ `)
