0fd40270083fbb442bf0bba0b85dfd2121844dc3
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-25 20:03:20 -0300

    Update site.js

--- site.js
+++ site.js
@@ -28,6 +28,7 @@ res.send(`
 <meta charset='utf-8'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel'stylesheet' href='//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css'>
 <title> LuuLBot - Comandos </title>
 <link rel='icon' href='https://cdn.frankerfacez.com/010a6a6829cfe953dbe1958557424bc4.png'>
 
@@ -83,14 +84,11 @@ ${commandList.join(' \n')}
 
 $(document).ready(function () {
   $('#commandsTable').DataTable();
-  $('.dataTables_length').addClass('bs-select');
 });
 
 </script>
 
-<script src='https://code.jquery.com/jquery-3.5.1.js'></script>
-<script src='https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js'></script>
-<script src='https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap.min.js'></script>
+<script src='//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js'></script>
 
 </body>
 
