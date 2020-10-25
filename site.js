1765edb8905c0280e6fc805549fdf37811a5acbf
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-25 20:00:34 -0300

    Update site.js

--- site.js
+++ site.js
@@ -88,6 +88,10 @@ $(document).ready(function () {
 
 </script>
 
+<script src='https://code.jquery.com/jquery-3.5.1.js'></script>
+<script src='https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js'></script>
+<script src='https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap.min.js'></script>
+
 </body>
 
 </html>
