e3c0fd8654cb2a45eae8f9bc40a28712430ae4eb
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-10-26 08:46:08 -0300

    Update site.js

--- site.js
+++ site.js
@@ -5,6 +5,7 @@ const app = express();
 const luulbot = require('./clients/discord.js').luulbot;
 const client = require('./clients/twitch.js').client;
 const db = require('./clients/database.js').db;
+const ms = require('pretty-ms');
 
 app.get('/comandos', (req, res) => {
   
@@ -196,6 +197,114 @@ color: #ffffff
 
 `)
 })
+
+app.get('/suggests', async (req, res) => {
+  
+ const suggestsList = await db.query('SELECT * FROM luulbot_suggests ORDER BY suggestid DESC');
+  
+ const suggestTable = suggestsList.map(i => `
+
+<tr>
+<td><a>${i.userchannel}</a></td>
+<td><a>${i.usersuggest}</a></td>
+<td><a>${i.status}</a></td>
+<td><a>${ms(Date.now() - new Date(i.suggestdate), {unitCount: 2, secondsDecimalDigits: 0})} atrás</a></td>
+<td><a>${i.suggestid}</a></td>
+</tr>
+
+`);
+ 
+  
+res.send(`
+<!DOCTYPE html>
+
+<html lang='pt' class='no-js'>
+
+<head>
+
+<meta charset='utf-8'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="https://bootswatch.com/4/darkly/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+<title> LuuLBot - Sugestões </title>
+<link rel='icon' href='https://cdn.frankerfacez.com/010a6a6829cfe953dbe1958557424bc4.png'>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
+
+
+</head>
+
+<body>
+
+<style>
+
+body {
+background-color: #222b36;
+}
+
+
+
+</style>
+
+<nav class='navbar navbar-expand navbar-dark bg-dark  navbar-fixed-top'>
+<a class='navbar-brand'>LuuLBot</a>
+<div class="collapse navbar-collapse" id="navbarNav">
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="/">Home</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="/comandos">Comandos</a>
+</li>
+</ul>
+</div>
+</nav>
+
+
+<div class='row justify-content-center'>
+
+
+<table id='suggestsTable' class='table table-dark table-striped table-bordered dataTable'>
+
+<thead>
+<tr>
+<td>USUÁRIO</td>
+<td>SUGESTÃO</td>
+<td>STATUS</td>
+<td>TEMPO</td>
+<td>ID</td>
+</tr>
+</thead>
+
+<tbody>
+${suggestTable.join(' \n')}
+</tbody>
+</table>
+</div>
+
+<script>
+
+$(document).ready(function () {
+  $('#suggestsTable').DataTable({
+        "language": {
+"url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json"
+}
+    });
+});
+
+</script>
+
+
+</body>
+
+</html>
+`)
+})
+
 
 
 app.listen(PORT, () => {
