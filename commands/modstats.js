59b2c5f733046a729264e532e9a3721ae2b882fa
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-10 11:55:40 -0300

    Update modstats.js

--- commands/modstats.js
+++ commands/modstats.js
@@ -1,27 +1,21 @@
 module.exports.run = async (client, message, args, username, channel) => {
 
 const fetch = require("node-fetch");
-const hastebin = require("easy-hastebin");
 
 const userid = (!args[0]) ? username : args[0];
   
-const res = await (await fetch(`https://modlookup.3v.fi/api/user-v3/${userid}`)).json();
-const res2 = await (await fetch(`https://modlookup.3v.fi/api/user-totals/${userid}`)).json();  
-  
-const modsList = res.channels.map(i => i.name)  
+const res = await (await fetch(`https://modlookup.3v.fi/api/user-totals/${userid}`)).json();  
  
  try { 
    
-const haste = await hastebin(modsList)  
-
-const {user, views, follows, total, partners} = res2;
+const {user, views, follows, total, partners} = res;
 
 const sender = (user === username) ? 'você' : user; 
    
 let channelTotal = total ? total > 1 ? `${total} canais` : `1 canal` : 'nenhum canal';
 let partnerTotal = partners ? partners > 1 ? `(${partners}  parceiros)` : '(1 parceiro)' : '';     
 
-return client.say(channel, `${username}, ${sender} é mod em ${channelTotal} ${partnerTotal} | Views totais: ${views} | Follows totais: ${follows} | Canais: ${haste}`)
+return client.say(channel, `${username}, ${sender} é mod em ${channelTotal} ${partnerTotal} | https://modlookup.3v.fi/u/${user}`)
    
 
  } catch (err) {
