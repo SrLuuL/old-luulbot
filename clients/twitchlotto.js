9b149bf2fa0b6ae80ad1483a3ec8da3d6f9414e4
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-16 18:19:37 -0300

    Update twitchlotto.js

--- clients/twitchlotto.js
+++ clients/twitchlotto.js
@@ -2,7 +2,7 @@ const { ChatClient } = require('dank-twitch-irc');
 
 const client = new ChatClient({
 	username: 'SrLuuL',
-	password: 'oauth:7e46sisoj4putjykwkm2m2r2qt16cq'
+	password: process.env.TWITCHLT_AUTH
 })
 
 const channels = ['srluul', 'ghiletofar', 'cellbit', 'alanzoka', 'smurfdomuca', 'gabi', 'ljoga', 'mount', 'patopapao', 'felps', 'omeiaum'] 
