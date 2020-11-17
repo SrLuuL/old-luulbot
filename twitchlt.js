376544f931cf85fa2cf24cd846e6a42b49ea0f18
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-17 12:28:54 -0300

    Update twitchlt.js

--- twitchlt.js
+++ twitchlt.js
@@ -7,10 +7,8 @@ client.on('PRIVMSG', async (msg) => {
 	const { messageText: message, displayName: username, channelID, senderUserID, channelName } = msg
 	
 	const imgRegex = /(http|https):\/\/(.*?)\.(imgur)\.(com)\/(.*?)\.(png|jpg|gif|PNG|JPG|GIF)/i
-	const ytRegex = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?‌​=]*)?/
 	
 	const imgValidation = imgRegex.exec(message);
-	const ytValidation = ytRegex.exec(message);
 	
 	const linkDatabase = await db.query('SELECT * FROM luulbot_twitchlt');
 	
@@ -24,13 +22,5 @@ client.on('PRIVMSG', async (msg) => {
 	
 	}
 	
-	if (ytValidation) {
-		
-		if (linkDatabase.rows.find(i => i.url === ytValidation[0])) return;
 	
-		const currentDate = new Date().toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'});
-		
-		await db.query('INSERT INTO luulbot_twitchlt (channel, channel_id, username, username_id, url, date) VALUES ($1,$2,$3,$4,$5,$6)', [channelName, channelID, username, senderUserID, ytValidation[0], new Date(currentDate)]);
-	
-	}
 })
