9152e6fbb4dccd03d64c79c6f35d328a314e2e47
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-17 18:34:41 -0300

    Update whitelist.js

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -4,17 +4,20 @@ const db = require('../clients/database.js').db;
 const luulbot = require('../clients/discord.js').luulbot;
 
 const channelList = await db.query(`SELECT * FROM luulbot_channels WHERE userchannel = '${args[0]}'`);
+const channelPerm = await db.query(`SELECT * FROM luulbot_perms WHERE channel = '${args[0]}' AND command = '${args[1]}'`)
+const cmd = luulbot.commands.get(args[1].toLowerCase()) || luulbot.commands.get(luulbot.aliases.get(args[1].toLowerCase()))
+const {userchannel, userid} = channelList.rows[0]
 
 if (!channelList.rowCount) {
 return client.say(channel, `${username}, bot não está presente neste canal :/`)
 }
 
-const {userchannel, userid} = channelList.rows[0]
-
-const cmd = luulbot.commands.get(args[1].toLowerCase()) || luulbot.commands.get(luulbot.aliases.get(args[1].toLowerCase()))
-
 if (!cmd) {
 return client.say(channel, `${username}, o bot não possui esse comando :/`)
+}
+  
+if (channelPerm.rowCount) {
+  return client.say(channel, `${username}, o canal já possui acesso a esse comando :/`)
 }
 
 await db.query(`INSERT INTO luulbot_perms(channel, channel_id, command) VALUES($1,$2,$3)`, [userchannel, userid, cmd.config.name])
