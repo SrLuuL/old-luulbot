8ce4fea117e2848d51521df66254a9b125f2b565
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-17 13:19:09 -0300

    Create twitchlotto.js

---
+++ commands/twitchlotto.js
@@ -0,0 +1,32 @@
+module.exports.run = async (client, message, args, username, channel) => {
+
+const db = require('./clients/database.js').db;
+const ms = require('pretty-ms');
+
+const twitchltDB = await db.query('SELECT * FROM luulbot_twitchlt');
+
+if (!twitchltDB.rows) {
+return client.say(channel, `${username}, não há imagens no momento :/`)
+}
+
+
+const fetchUrls = twitchltDB.rows.map(i => i.url);
+
+let randomUrl = fetchUrls[Math.round(Math.random() * fetchUrls.length)];
+let randomUrlDate = twitchltDB.rows.find(i => i.url === randomUrl)
+
+randomUrlDate = ms(Date.now() - randomUrlDate.date, {secondsDecimalDigits: 0, unitCount: 3})
+.replace(/y/, 'a');
+
+client.say(channel, `${username}, ${randomUrl} (Postada há ${randomUrlDate} atrás)`) 
+
+
+}
+
+module.exports.config = {
+name: 'twitchlotto',
+aliases: ['twitchlt', 'lt'],
+usage: 'twitchlotto',
+description: 'Pega uma imagem aleatória de chats brasileiros',
+level: 'Privado'
+}
