1561f2dee0428f1a697541bfbcf532021af09f86
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-17 22:25:46 -0300

    Update channelslt.js

--- data/channelslt.js
+++ data/channelslt.js
@@ -1,6 +1,16 @@
 module.exports = [
-
-
-
-
+  'ghiletofar',
+  'cellbit',
+  'alanzoka',
+  'gabi',
+  'calango',
+  'rakin',
+  'ljoga',
+  'akkaiverso',
+  'mount',
+  'felps',
+  'patopapao',
+  'omeiaum',
+  'smurfdomuca',
+  'namaca'
 ]
