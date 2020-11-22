824cff7fe6b6a065b0a7abcee8c10962a0914ce6
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-21 21:48:55 -0300

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,16 @@
+module.exports.run = (client, message, args, username, channel) => {
+
+return {
+reply: 'sim!!!'
+}
+
+}
+
+module.exports.config = {
+name: 'test',
+aliases: [],
+description: 'teste',
+usage: null,
+cooldown: 500
+level: 'Todos'
+}
