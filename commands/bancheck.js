259aff3c60ef1e0a9e6149b1588bbd0a8b4b7f9b
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-21 23:29:07 -0300

    Update bancheck.js

--- commands/bancheck.js
+++ commands/bancheck.js
@@ -1,11 +1,13 @@
-module.exports.run = async (client, message, args, username, channel) => {
+module.exports.run = async (args, username, channel) => {
 
 const ms = require("pretty-ms");
 const fetch = require("node-fetch");
 
 let [user, user2] = args;
 
-if (!user) return client.say(channel, `${username}, insira usuários :/`)
+if (!user) {
+ return { reply: 'insira usuários :/' } 
+}
 
 if (!user2) {
 user = username
@@ -14,15 +16,23 @@ user2 = args[0]
 
 const res = await (await fetch(`https://api.ivr.fi/twitch/banlookup/${user}/${user2}`)).json(); 
   
-if (res.error) return client.say(channel, `${username}, conta inexistente ou suspensa`);  
+if (res.error) {
+ return { reply: 'conta inexistente ou suspensa' } 
+}
+  
 const {status, banned, isPermanent, createdAt, expiresAt} = res;
 
 user = (user.toLowerCase() === username) ? "você" : user;
 user2 = (user2.toLowerCase() === username) ? "seu canal" : user2;
 
-if (status === 500) return client.say(channel, `${username}, usuários inválidos :/`)
-if (!banned) return client.say(channel, `${username}, ${user} não está com um ban em ${user2}`)
-
+if (status === 500) {
+  return { reply: 'usuários inválidos :/' }
+}
+  
+if (!banned) {
+  return { reply: `${user} não está com um ban em ${user2}` }
+}
+  
 const dateBan = ms(Date.now() - new Date(createdAt), {secondsDecimalDigits: 0, unitCount: 3})
 .replace(/y/g, "a");
 const dateExpire = ms(new Date(expiresAt) - Date.now(), {secondsDecimalDigits: 0, unitCount: 3})
@@ -31,9 +41,9 @@ const dateExpire = ms(new Date(expiresAt) - Date.now(), {secondsDecimalDigits: 0
 
   
 if (isPermanent) {
-client.say(channel, `${username}, ${user} está com um ban permanente em ${user2} há ${dateBan} atrás`)
+return { reply: `${user} está com um ban permanente em ${user2} há ${dateBan} atrás` }
 } else {
-client.say(channel, `${username}, ${user} está com timeout em ${user2} há ${dateBan} atrás e vai acabar em ${dateExpire}`)
+return { reply: `${user} está com timeout em ${user2} há ${dateBan} atrás e vai acabar em ${dateExpire}` }
 }
 
 
