daead7703fbc4a80a62e34666c15453fafffe089
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-22 00:04:22 -0300

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,18 +1,15 @@
-module.exports.run = async (args) => {
+module.exports.run = async (context) => {
 
   const db = require('../clients/database.js').db
 
 
   try {
   
-let content = args.join(" ")
-
-
-const evaluated = await eval('(async () => {' +args.join(" ") + '})()')
+const evaluated = await eval('(async () => {' +context.args.join(" ") + '})()')
 const ev = String(evaluated)
 
 
-if (!args[0]) return { reply: 'insira um código :/' }
+if (!context.args[0]) return { reply: 'insira um código :/' }
 else return { reply: `${ev}` }
 
     
