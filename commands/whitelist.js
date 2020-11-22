511c65b830324d050bab8d0b5bbb3eaa99ee0eb3
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-22 00:40:16 -0300

    Update whitelist.js

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -1,28 +1,31 @@
-module.exports.run = async (client, message, args, username, channel) => {
+module.exports.run = async (context) => {
 
 const db = require('../clients/database.js').db;
 const luulbot = require('../clients/discord.js').luulbot;
-
+const {args} = context
+  
 const channelList = await db.query(`SELECT * FROM luulbot_channels WHERE userchannel = '${args[0]}'`);
 const channelPerm = await db.query(`SELECT * FROM luulbot_perms WHERE channel = '${args[0]}' AND command = '${args[1]}'`)
 const cmd = luulbot.commands.get(args[1].toLowerCase()) || luulbot.commands.get(luulbot.aliases.get(args[1].toLowerCase()))
 const {userchannel, userid} = channelList.rows[0]
 
 if (!channelList.rowCount) {
-return client.say(channel, `${username}, bot não está presente neste canal :/`)
+return { reply: 'bot não está presente neste canal :/' }
 }
 
 if (!cmd) {
-return client.say(channel, `${username}, o bot não possui esse comando :/`)
+return { reply: 'bot não possui este comando :/' }
 }
   
 if (channelPerm.rowCount) {
-  return client.say(channel, `${username}, o canal já possui acesso a esse comando :/`)
+  return { reply: 'o canal já possui acesso a este comando :/' }
 }
 
 await db.query(`INSERT INTO luulbot_perms(channel, channel_id, command) VALUES($1,$2,$3)`, [userchannel, userid, cmd.config.name])
 
-client.say(channel, `${username}, ${userchannel} agora possui ${cmd.config.name}!`)
+return {
+ reply: `${userchannel} agora possui ${cmd.config.name}!` 
+}
 
 }
 
