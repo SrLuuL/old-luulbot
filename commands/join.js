1fbffab5ef94e9fdb0d6b3254423c931b6b5a528
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-22 00:05:49 -0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,19 +1,19 @@
-module.exports.run = async (args) => {
+module.exports.run = async (context) => {
   
 const channelsOptions = require("../credentials/login.js").channelOptions
 const db = require("../clients/database.js").db
 
 
-if (!args[0]) return { reply: 'nenhum canal citado :/' }
+if (!context.args[0]) return { reply: 'nenhum canal citado :/' }
  
 const getId = await db.query('SELECT userid FROM luulbot_channels ORDER BY userid ASC')  
 
-db.query(`INSERT INTO luulbot_channels(userchannel, userid) VALUES('${args[0]}', '${getId.rows.pop().userid + 1}')`);
-client.join(args[0]);
-channelsOptions.push(`${args[0]}`)
+db.query(`INSERT INTO luulbot_channels(userchannel, userid) VALUES('${context.args[0]}', '${getId.rows.pop().userid + 1}')`);
+context.client.join(context.args[0]);
+channelsOptions.push(`${context.args[0]}`)
 
 return {
- reply: `conectado com sucesso em ${args[0]}` 
+ reply: `conectado com sucesso em ${context.args[0]}` 
 }
   
 }
