aea759b4b4d05b0d8dad60cb90cda11f1f4df51d
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-11-21 23:44:31 -0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,15 +1,21 @@
-module.exports.run = async (client, message, args, username, channel) => {
+module.exports.run = async (args) => {
+  
 const channelsOptions = require("../credentials/login.js").channelOptions
 const db = require("../clients/database.js").db
 
 
-if (!args[0]) return client.say(channel, `${username}, nenhum canal no input :Z`)
+if (!args[0]) return { reply: 'nenhum canal citado :/' }
+ 
 const getId = await db.query('SELECT userid FROM luulbot_channels ORDER BY userid ASC')  
 
 db.query(`INSERT INTO luulbot_channels(userchannel, userid) VALUES('${args[0]}', '${getId.rows.pop().userid + 1}')`);
 client.join(args[0]);
 channelsOptions.push(`${args[0]}`)
-client.say(channel, `${username}, entrei no canal ${args[0]} com sucesso!`)
+
+return {
+ reply: `conectado com sucesso em ${args[0]}` 
+}
+  
 }
 
 module.exports.config = {
