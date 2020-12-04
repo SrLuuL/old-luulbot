9a947e24d2e0bae25668185242ea2890ece639c3
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-12-04 19:21:16 -0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -8,7 +8,7 @@ if (!context.args[0]) return { reply: 'nenhum canal citado :/' }
  
 const getId = await db.query('SELECT userid FROM luulbot_channels ORDER BY userid ASC')  
 
-db.query(`INSERT INTO luulbot_channels(userchannel, userid) VALUES('${context.args[0]}', '${getId.rows.pop().userid + 1}')`);
+db.query(`INSERT INTO luulbot_channels(userchannel, userid, useruid) VALUES($1,$2,$3)`, [context.args[0], getId.rows.pop().userid + 1, context.user['user-id']]);
 context.client.join(context.args[0]);
 channelsOptions.push(`${context.args[0]}`)
 
