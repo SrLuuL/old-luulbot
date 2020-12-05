ee44b9806d9442803d8c3b32fc4a0ff44ff98083
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-12-04 21:02:12 -0300

    Update twitchpub.js

--- events/twitchpub.js
+++ events/twitchpub.js
@@ -87,7 +87,7 @@ async function handleWSMsg(msg = {}) {
 
 async function listenStreamStatus(channel) {
 	
-	let tokenDB = await db.query('SELECT access_token FROM luulbot_info ');
+	let tokenDB = await db.query(`SELECT setting FROM luulbot_info WHERE setting = 'access_token' `);
 	let token = tokenDB.rows[0].access_token;
 	
 	let nonce = randombytes(20).toString('hex').slice(-8);
