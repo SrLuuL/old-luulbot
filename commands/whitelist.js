81aceedcc550c09624de12732e61db185b3ad931
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-12-05 17:54:15 -0300

    Update whitelist.js

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -7,7 +7,7 @@ const {args} = context
 const channelList = await db.query(`SELECT * FROM luulbot_channels WHERE userchannel = '${args[0]}'`);
 const channelPerm = await db.query(`SELECT * FROM luulbot_perms WHERE channel = '${args[0]}' AND command = '${args[1]}'`)
 const cmd = luulbot.commands.get(args[1].toLowerCase()) || luulbot.commands.get(luulbot.aliases.get(args[1].toLowerCase()))
-const {userchannel, userid} = channelList.rows[0]
+const {userchannel, userid, useruid} = channelList.rows[0]
 
 if (!channelList.rowCount) {
 return { reply: 'bot não está presente neste canal :/' }
@@ -21,7 +21,7 @@ if (channelPerm.rowCount) {
   return { reply: 'o canal já possui acesso a este comando :/' }
 }
 
-await db.query(`INSERT INTO luulbot_perms(channel, channel_id, command) VALUES($1,$2,$3)`, [userchannel, userid, cmd.config.name])
+await db.query(`INSERT INTO luulbot_perms(channel, channel_id, command, channel_uid) VALUES($1, $2, $3, $4)`, [userchannel, userid, cmd.config.name, useruid])
 
 return {
  reply: `${userchannel} agora possui ${cmd.config.name}!` 
