1a8c21e68d441d9192ea1c98c3fff9c1da1a0e1e
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-12-07 09:19:08 -0300

    Update interval_functions.js

--- events/interval_functions.js
+++ events/interval_functions.js
@@ -1,1 +1,20 @@
+const db = require('../clients/database.js').db;
+const channels = require('../credentials/login.js').channelOptions;
+const client = require('../clients/twitch.js').client;
 
+client.Channels = new Map();
+
+setInterval(async () => {
+  
+  channels.forEach(channel => {
+    
+   let channelDB = await db.query(`SELECT * FROM luulbot_channels WHERE userchannel = '${channel}'`);
+   let { userchannel, userid, useruid, status, mode, stream_mode } = channelDB.rows[0]
+   
+   client.Channels.set(channel, { channel: userchannel, id: userid, uid: useruid, status: status, mode: mode, stream_mode: stream_mode } );
+    
+  })
+  
+  
+  
+}, 1800000)
