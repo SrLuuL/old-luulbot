f535856a236a4ebc63ef1135565bc5db2a929ce6
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-12-07 08:57:26 -0300

    Update functions.js

--- events/functions.js
+++ events/functions.js
@@ -1,19 +1,42 @@
 const client = require('../clients/twitch.js').client;
 const fetch = require('node-fetch');
+const db = require('../clients/database.js').db;
+const channels = require('../credentials/login.js').channelOptions;
 
-
- client.getEmotes = async (channel) => {
+client.getEmotes = async (channel) => {
   
 const emotes = [];
    
+ try {
+ 
 const ffzEmotes = await (await fetch(`https://decapi.me/ffz/emotes/${channel}`)).text()
 const bttvEmotes = await (await fetch(`https://decapi.me/bttv/emotes/${channel}`)).text()
 const twitchEmotes = await (await fetch(`https://decapi.me/twitch/subscriber_emotes/${channel}`)).text()
 
 emotes.push({ ffz: ffzEmotes, bttv: bttvEmotes, twitch: twitchEmotes })
   
-return emotes[0];  
+return emotes[0]; 
+  
+ } catch (err) {
+  return 'Function Err!'
+ }
   
 }
 
+client.getChannel = async (channel) => {
+ 
+ try {
+ 
+ let channelDB = await db.query(`SELECT * FROM luulbot_channels WHERE userchannel = '${channel}'`);
+ 
+ let channelInfo = channelDB.rows[0]
+
+ return channelInfo
+ 
+ } catch(err) {
+  return 'Function Err!'
+ }
+ 
+}
+ 
 
