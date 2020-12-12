09e02306eb1c559fb8527969c6fd59bdac4b9fd5
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-12-12 00:18:31 -0300

    Update modostream.js

--- commands/modostream.js
+++ commands/modostream.js
@@ -50,6 +50,6 @@ name: 'modostream',
 aliases: ['streammode', 'sm'],
 description: 'Ativa ou desativa o modo stream. O modo stream fará com que o bot não responda mais comandos durante lives',
 usage: 'modostream [on/off]',
-cooldown: 10000,
+cooldown: 5000,
 level: 'Moderador'
 }
