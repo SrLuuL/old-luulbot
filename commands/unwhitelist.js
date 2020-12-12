ad36755d0f6198594c0f515605f2a39d67383231
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2020-12-12 00:09:08 -0300

    Update unwhitelist.js

--- commands/unwhitelist.js
+++ commands/unwhitelist.js
@@ -7,7 +7,6 @@ const {args} = context
 const channelList = await db.query(`SELECT * FROM luulbot_channels WHERE userchannel = '${args[0]}'`);
 const channelPerm = await db.query(`SELECT * FROM luulbot_perms WHERE channel = '${args[0]}' AND command = '${args[1]}'`)
 const cmd = luulbot.commands.get(args[1].toLowerCase()) || luulbot.commands.get(luulbot.aliases.get(args[1].toLowerCase()))
-const {userchannel, userid} = channelList.rows[0]
 
 if (!channelList.rowCount) {
 return { reply: 'bot não está presente neste canal :/' }
@@ -20,6 +19,9 @@ return { reply: 'bot não possui esse comando :/' }
 if (!channelPerm.rowCount) {
   return { reply: 'o canal não possui acesso a este comando :/' }
 }
+
+const {userchannel, userid} = channelList.rows[0];
+
 
 await db.query(`DELETE FROM luulbot_perms WHERE channel = '${args[0]}' AND command = '${args[1]}'`)
 
