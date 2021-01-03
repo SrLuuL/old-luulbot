ff064c0cb905c59a648d3bf34f72da2232fc7c91
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-01-03 02:32:46 -0300

    Update site.js

--- site.js
+++ site.js
@@ -297,14 +297,14 @@ color: #ffffff
 
 app.get('/suggests/', async (req, res) => {
 	
-  
+ 
  const suggestsList = await db.query('SELECT * FROM luulbot_suggests ORDER BY suggestid DESC');
  const currentTime  = new Date(new Date().toLocaleString('pt-br', {timeZone: 'America/Bahia'}));
  const suggestTable = suggestsList.rows.map(i => `
-
+ const suggestK  = xss(i.usersuggest);
 <tr>
 <td><a>${i.userchannel}</a></td>
-<td><a>${xss(i.usersuggest)}</a></td>
+<td><a>${suggestK}</a></td>
 <td><a>${i.status}</a></td>
 <td><a>${moment(i.suggestdate).locale('pt').fromNow()} atrás</a></td>
 <td><a href='/suggests/${i.suggestid}'>${i.suggestid}</a></td>
@@ -431,6 +431,7 @@ app.get('/suggests/:id', async (req, res) => {
 	
 	
  const suggestTable = suggestsList.rows[0];
+ const suggestK = xss(suggestTable.usersuggest);
  
   
 res.send(`
@@ -511,7 +512,7 @@ td {
 </tr>
 <tr>
 <td>SUGESTÃO</td>
-<td>${xss(suggestTable.usersuggest)}</td>
+<td>${suggestK}</td>
 </tr>
 <tr>
 <td>STATUS</td>
