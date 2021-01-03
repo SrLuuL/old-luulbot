b737ff5013de54dffbf9dff2b46180f1d7e7b4a9
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-01-03 02:57:33 -0300

    Update site.js

--- site.js
+++ site.js
@@ -7,7 +7,7 @@ const client = require('./clients/twitch.js').client;
 const db = require('./clients/database.js').db;
 const ms = require('pretty-ms');
 const moment = require('moment');
-const sani  = require('sanitizer')
+
 
 const wrongPage = `
 <!DOCTYPE html>
@@ -304,7 +304,7 @@ app.get('/suggests/', async (req, res) => {
  
 <tr>
 <td><a>${i.userchannel}</a></td>
-<td><a>${sani.escape(i.suggest)}</a></td>
+<td><a>${i.suggest}</a></td>
 <td><a>${i.status}</a></td>
 <td><a>${moment(i.suggestdate).locale('pt').fromNow()} atrás</a></td>
 <td><a href='/suggests/${i.suggestid}'>${i.suggestid}</a></td>
@@ -512,7 +512,7 @@ td {
 </tr>
 <tr>
 <td>SUGESTÃO</td>
-<td>${sani.escape(suggestTable.suggest)}</td>
+<td>${suggestTable.suggest}</td>
 </tr>
 <tr>
 <td>STATUS</td>
