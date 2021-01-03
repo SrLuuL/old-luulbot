7ad9dcbadb574e53fcf95e61e96937f4f5465d18
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-01-03 03:14:25 -0300

    Update site.js

--- site.js
+++ site.js
@@ -319,7 +319,7 @@ res.send(`
 <html lang='pt' class='no-js'>
 
 <head>
-
+<META http-equiv="Content-Type" content="text/html; charset= ISO-8859-1"> 
 <meta charset='utf-8'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
@@ -441,6 +441,7 @@ res.send(`
 
 <head>
 
+<META http-equiv="Content-Type" content="text/html; charset= ISO-8859-1"> 
 <meta charset='utf-8'>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
