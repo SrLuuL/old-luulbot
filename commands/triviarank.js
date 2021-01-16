b432ef0d785ab69486b65155cccc6e8b613d6931
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-01-16 17:35:55 -0300

    Update triviarank.js

--- commands/triviarank.js
+++ commands/triviarank.js
@@ -5,20 +5,21 @@ const {args, user} = context;
  
 let sender = (args[0]) ? args[0].toLowerCase() : user.username.toLowerCase();
 const triviaDB = await db.query(`SELECT * FROM luulbot_trivia WHERE user_name = '${sender}'`);
-const triviaTopDB =  await db.query(`SELECT * FROM luulbot_trivia ORDER BY user_points DESC`);
+const triviaPlaceDB = await db.query(`SELECT ROW_NUMBER() OVER(ORDER BY user_points DESC) AS Rank, user_points FROM luulbot_trivia`) 
  
 if(sender === 'top') {
  return { reply: 'Leaderboard em construção 🔧 ' }
 }
 
-let triviaPlace =  triviaTopDB.rows.indexOf(sender);
  
 sender = (sender === user.username) ? 'você' : sender; 
  
 if (!triviaDB.rowCount) {
  return { reply: `${sender} não acertou nenhuma trivia`}
 } else {
- return { reply: `${sender} já acertou ${triviaDB.rows[0].user_points} trivia(s)! [${triviaPlace + 1}° Lugar]` }
+ let triviaPoints = triviaDB.rows[0].user_points;
+ let triviaPlace =  triviaPlaceDB.find(i => i['user_points'] === triviaPoints).rank;
+ return { reply: `${sender} já acertou ${triviaPoints} trivia(s)! [${triviaPlace + 1}° Lugar]` }
  }
 
 }
