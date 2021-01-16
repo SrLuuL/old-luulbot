ce1d65b969f2621de0044bf2c6f58b7a42320e0b
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-01-16 14:28:03 -0300

    Update and rename twitchlotto.js to trivia.js

---
+++ commands/trivia.js
@@ -0,0 +1,96 @@
+const trivia = {
+  running: false,
+  stopped: true
+}
+
+module.exports.run = async (context) => {
+
+  const {user, args, client, channel} = context;
+  const triviaLength = args[1];
+  const questions = require('../data/trivia.json');
+  const db = require('../clients/database.js').db;
+  const compare = require('compare-strings');
+ 
+const sleep = (ms) => {
+  return new Promise(res => setTimeout(res, ms));
+}
+  
+async function triviaStart(questions) {
+  
+  if (args[0] === 'start' && !trivia.running) {
+    
+    trivia.running = true;
+    trivia.stopped = false;
+    
+    try {
+      
+      for(let i = 0; i < triviaLength; i++) {
+        
+        if(!trivia.running || trivia.stopped) {
+          break
+        }
+        
+        const randomQuestion = questions[(Math.random() * questions.length)];
+        const {category, question, answer} = randomQuestion;
+        
+        await client.say(channel, `[${i}/${triviaLength}] | Pergunta: ${question} | Categoria: ${category}`)
+        
+        const done = new Promise(res => {
+          
+          const timer = setTimeout(() => {
+           client.say(channel, `Ninguém acertou :/ | Resposta: ${answer}`);
+           res()
+          }, 15000)
+          
+          
+          client.addEventListener('message', async function triviaVerifier(channel, user, message){
+                                 
+            const similar = compare(message, answer);
+            
+            if (similar < 0.9) return;
+            
+            clearTimeout(timer)
+            
+            client.removeEventListener('message', triviaVerifier(channel, user, message));
+            client.say(channel, `${user.username} acertou! | Resposta: ${answer}`);
+            
+            const triviaDB = await db.query(`SELECT * FROM luulbot_trivia WHERE user = '${user.username}'`);
+            
+            if (!triviaDB.rowCount) {
+            await db.query(`INSERT INTO luulbot_trivia(user, user_id, user_points) VALUES($1, $2, $3) WHERE user = '${user.username}'`, [user.username, user['user-id'], 1])
+            } else {
+            await db.query(`INSERT INTO luulbot_trivia(user_points) VALUES(user_points+1) WHERE user = '${user.username}'`)
+            }
+            res()            
+                                 
+            })
+          
+        })
+        
+        await done;
+        await sleep(7000)
+        
+      } finally {
+        client.say(channel, 'Trivia acabou :Z');
+        trivia.running = false
+      } else if(args[0] === 'off') {
+        trivia.stopped = true
+      }
+  }
+  
+}
+
+  return { 
+    reply: await triviaStart() 
+  }
+  
+}
+
+module.exports.config = {
+name: 'trivia',
+aliases: [],
+usage: 'trivia [start/off]',
+description: 'Começa/termina uma trivia com perguntas de diferentes categorias(solicite permissão caso queira o comando).',
+level: 'Privado',
+cooldown: 6000    
+}
