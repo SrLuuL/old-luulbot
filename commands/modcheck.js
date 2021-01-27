040c6eeb9c0a18628d2479cc8ce5f54e21553cc0
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-01-27 17:10:53 -0300

    Update modcheck.js

--- commands/modcheck.js
+++ commands/modcheck.js
@@ -8,10 +8,6 @@ let user;
 
 const {args, user: username, channel} = context  
  
-if(!channel || !args[0]) {
-  return { reply: 'insira pelo menos um usuário :/' }
-}
-
 if (args[0] && args[1]) {
 sender = args[1].toLowerCase();
 user = args[0].toLowerCase();  
