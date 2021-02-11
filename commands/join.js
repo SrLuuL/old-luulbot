304190fb8a958e661e5713d4426cbbcf58cc28ac
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-02-11 18:52:16 -0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -8,16 +8,23 @@ let channelSender = context.args[0];
 
 if (!channelSender) return { reply: 'nenhum canal citado :/' }
  
-const getId = await db.query('SELECT userid FROM luulbot_channels ORDER BY userid ASC')
+const getUID = await db.query('SELECT useruid FROM luulbot_channels ORDER BY useruid ASC')
+const getID = await db.query('SELECT userid FROM luulbot_channels ORDER BY userid ASC')
 const userID = await (await fetch(`https://luulbot.herokuapp.com/api/twitch/user/${channelSender}`)).json()
 
 if(userID.error) {
  return { reply: `${userID.error} :/` } 
 }
+ 
+if(getID.rows.find(i => i.userid === userID.id)) {
+ await db.query(`UPDATE luulbot_channels SET userchannel = '${channelSender}'`);
+} else {
 
-await db.query(`INSERT INTO luulbot_channels(userchannel, userid, useruid) VALUES($1,$2,$3)`, [channelSender, getId.rows.pop().userid + 1, userID.id]);
+await db.query(`INSERT INTO luulbot_channels(userchannel, useruid, userid) VALUES($1,$2,$3)`, [channelSender, getUID.rows.pop().userid + 1, userID.id]);
 context.client.join(channelSender);
 channelsOptions.push(`${channelSender}`)
+
+}
 
 return {
  reply: `conectado com sucesso em ${channelSender}` 
