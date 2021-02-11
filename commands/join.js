4f63230401526c6c9820fb20b2d2cf7ce3531df3
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-02-11 19:58:10 -0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -18,9 +18,9 @@ if(userID.error) {
 }
  
 let channelInfo =  channelsDB.rows.find(i => i.userid === userID.id);
+console.log(channelInfo) 
  
 if(channelInfo) {
- console.log('cu')
  await db.query(`UPDATE luulbot_channels SET userchannel = '${channelSender}' WHERE userid = '${channelInfo.userid}'`);
  const channelPlace = channelsOptions.findIndex(i => i === channelSender);
  channelsOptions.splice(`${channelInfo.userchannel}`, 1);
