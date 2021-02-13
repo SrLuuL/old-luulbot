e5afd4edfeb338ed8d1f0f551228de4f99ac354d
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-02-13 17:37:22 -0300

    Update part.js

--- commands/part.js
+++ commands/part.js
@@ -1,8 +1,8 @@
-module.exports.run = async (context) => {
+module.exports.run = async ({args, client}) => {
 const channels = require("../credentials/login.js").channelOptions;
 const db = require("../clients/database.js").db
-let channelsList = context.client.channels
-const {args} = context
+let channelsList = client.channels
+
 
 if (!args[0]) return { reply: 'insira um canal :/' }
 if (!channels.includes(args[0])) return { reply: 'não estou nesse canal :/' }
@@ -16,7 +16,7 @@ channels.splice(index, 1)
   
   
 if (channelsList.includes(`#${args[0]}`)) {
-    context.client.part(args[0]);
+    client.part(args[0]);
 }
   
 
