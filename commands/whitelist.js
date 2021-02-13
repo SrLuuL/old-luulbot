27b3713a65cb7c81a786878e22a268e4c9c29510
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-02-13 17:45:51 -0300

    Update whitelist.js

--- commands/whitelist.js
+++ commands/whitelist.js
@@ -2,7 +2,6 @@ module.exports.run = async ({args}) => {
 
 const db = require('../clients/database.js').db;
 const luulbot = require('../clients/discord.js').luulbot;
-const {args} = context  
 const channelList = await db.query(`SELECT * FROM luulbot_channels WHERE userchannel = '${args[0]}'`);
 const channelPerm = await db.query(`SELECT * FROM luulbot_perms WHERE channel = '${args[0]}' AND command = '${args[1]}'`)
 const cmd = luulbot.commands.get(args[1].toLowerCase()) || luulbot.commands.get(luulbot.aliases.get(args[1].toLowerCase()))
