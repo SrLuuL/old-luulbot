606f2f76de9880ebaf1384e275a637f9313dd3b1
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-14 18:43:27 -0300

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -1,7 +1,30 @@
-module.exports.run = async ({args, response}) => {
+module.exports.run = async ({args, response, user}) => {
   
+  const db = require('../clients/database.js').db;
   
+  let afkMessage = `${user.username} está AFK:`;
+  let currentDate = Date.now();
+  let message = (!args[0]) ? '(sem mensagem)' : args.join(' ');
+  
+  switch(response) {
+    case 'gn':
+      afkMessage = `${user.username} foi dormir:`
+      message += ' 💤';
+      break;
+    case 'study':
+      afkMessage = `${user.username} foi estudar:`
+      message += ' 📚';
+    case 'shower':
+      afkMessage = `${user.username} foi tomar banho:`
+      message += ' 🚿';
+  }
+  
+  await db.query(`INSERT INTO luulbot_afk(channel, reason, afk, time) VALUES($1, $2, $3, $4)`, [user.username, message, response, currentDate])
   
 }
 
+module.exports.config = {
+ name: 'afk',
+ aliases: ['gn', 'study', 'shower']
+}
 
