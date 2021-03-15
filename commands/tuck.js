2960c515d624d689d0f800de4ffcd9fd81d40707
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-14 23:20:44 -0300

    Update tuck.js

--- commands/tuck.js
+++ commands/tuck.js
@@ -1,1 +1,24 @@
 
+module.exports.run = ({args}) => {
+  
+  if(!args[0]) {
+   return { reply: `você botou si mesmo para dormir :/` } 
+  }
+  
+  let emote = (!args[1]) ? ':)' : args[1];
+  
+  return {
+   reply: `você botou ${args[0]} para dormir ${emote} 👉 🛏️` 
+  }
+  
+}
+
+
+module.exports.config = {
+ name: 'tuck',
+ aliases: [''],
+ description: 'Bote alguém para dormir',
+ usage: 'tuck [user]',
+ cooldown: 3000,
+ level: 'Todos'
+}
