dff018a6b3cabb9d7398955203c4fde1c7e73023
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-16 11:08:19 -0300

    Update check.js

--- commands/check.js
+++ commands/check.js
@@ -1,16 +1,19 @@
-module.exports.run = async ({args}) => {
+module.exports.run = async ({args, user}) => {
   
   const db = require('../clients/database.js').db;
   const ms = require('pretty-ms');
   
   switch(args[0]) {
     case 'afk':
-      
       try {
       
       if(!args[1]) return { reply: `insira um usuário para checar seu AFK :/` };
-      
-      let afkCheck = await db.query(`SELECT * FROM luulbot_afk WHERE username = '${args[1].toLowerCase()}'`);
+      let afkSender = args[1].toLowerCase();
+        
+      if(afkSender === user.username) return { reply: `eu acho que você não está afk O_o` };
+      if(afkSender === 'luulbot') return { reply: `estou sempre aqui B)` };  
+        
+      let afkCheck = await db.query(`SELECT * FROM luulbot_afk WHERE username = '${afkSender}'`);
       
       if(afkCheck.rows[0]) {
        let {time, reason, username} = afkCheck.rows[0];
