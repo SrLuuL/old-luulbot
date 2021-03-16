1a63a4746a7c5edee3d7dbba5eece5ad0469ee34
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-16 20:06:13 -0300

    Update resumeafk.js

--- commands/resumeafk.js
+++ commands/resumeafk.js
@@ -1,1 +1,31 @@
-modue
+module.exports.run = async ({client, user}) => {
+  
+  const db = require('../clients/database.js').db;
+  const afkList = client.afkList;
+
+  
+  let afkSearch = afkList.find(i => i.username === user.username);
+  
+  if(!afkSearch) {
+    
+   return { reply: 'nenhum AFK feito há menos de 5 minutos atrás :/' } 
+    
+  } else {
+   
+  let { username, reason, afk, time, channel, afkType, afkMessage } = afkSearch;
+  await db.query(`INSERT INTO luulbot_afk(username, reason, afk, time, channel, afktype) VALUES($1, $2, $3, $4, $5, $6)`, [user.username, reason, afk, time, channel, afkType])
+  
+    return { reply: `${afkMessage} ${reason}`, mode: 'say' }
+    
+  }
+  
+}
+
+module.exports.config = {
+ name: 'resumeafk',
+ aliases: ['rafk'],
+ description: 'Resume seu AFK se ele foi feito há menos de 5 minutos atrás',
+ usage: 'resumeafk',
+ cooldown: 5000,
+ level: 'Todos'
+}
