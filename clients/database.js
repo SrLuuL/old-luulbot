3606be48970c90c5d739ff349ae1b61673fed408
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-16 08:44:10 -0300

    Update database.js

--- clients/database.js
+++ clients/database.js
@@ -1,17 +1,10 @@
 const { Client } = require("pg");
 const connectionURI = process.env.DATABASE_URLL;
-const options = {
- user: 'waanqlbezgjabs',
- password: 'f4e8c028930a2b89dd87a73d2090c0c24e81e6ccf8ee75f8fa985b60658483d2',
- database: 'dd7rfjbn2rp2uo',
- port: 5432,
- host: 'ec2-52-1-95-247.compute-1.amazonaws.com',
- ssl: true
-}
 
 
 
-const db = new Client(options)
+
+const db = new Client({connectionString: connectionURI })
 
 db.connect()
 .catch(err => {console.log(err)});
