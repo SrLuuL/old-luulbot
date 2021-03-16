a2e0229215414494bb4aa08500c98a3d590d5093
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-16 20:25:30 -0300

    Update resumeafk.js

--- commands/resumeafk.js
+++ commands/resumeafk.js
@@ -1,22 +1,30 @@
-module.exports.run = async ({client, user}) => {
+module.exports.run = async ({client, user, msgType}) => {
   
   const db = require('../clients/database.js').db;
   const afkList = client.afkList;
 
+  if(msgType === 'whisper') {
+   return { reply: 'comando não está disponível em whispers!' } 
+  }
   
   let afkSearch = afkList.find(i => i.username === user.username);
+  let afkStatus = await db.query(`SELECT * FROM luulbot_afk WHERE username = '${user.username}'`);
   
   if(!afkSearch) {
     
    return { reply: 'nenhum AFK feito há menos de 5 minutos atrás :/' } 
     
-  } else {
+  } else if(afkStatus.rows[0]) {
    
-  let { username, reason, afk, time, channel, afkType, afkMessage } = afkSearch;
-  await db.query(`INSERT INTO luulbot_afk(username, reason, afk, time, channel, afktype) VALUES($1, $2, $3, $4, $5, $6)`, [user.username, reason, afk, time, channel, afkType])
-  
-    return { reply: `${afkMessage} ${reason}`, mode: 'say' }
+    return { reply: 'você já estava AFK O_o ' }
     
+  } else {
+    
+    
+     let { username, reason, afk, time, channel, afkType, afkMessage } = afkSearch;
+     await db.query(`INSERT INTO luulbot_afk(username, reason, afk, time, channel, afktype) VALUES($1, $2, $3, $4, $5, $6)`, [user.username, reason, afk, time, channel, afkType])
+  
+     return { reply: `${afkMessage} ${reason}`, mode: 'say' } 
   }
   
 }
