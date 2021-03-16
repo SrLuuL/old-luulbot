dc124e58d958efa24a74d795cb0136e8e078f6bf
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-16 08:56:28 -0300

    Update database.js

--- clients/database.js
+++ clients/database.js
@@ -1,12 +1,10 @@
-const pg = require('pg');
-pg.defaults.ssl = true
 const { Client } = require("pg");
 const connectionURI = process.env.DATABASE_URLL;
 
 
 
 
-const db = new Client({connectionString: connectionURI })
+const db = new Client({connectionString: connectionURI, ssl: {rejectUnauthorized: false}})
 
 db.connect()
 .catch(err => {console.log(err)});
