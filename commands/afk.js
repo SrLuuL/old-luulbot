8746e92a4f66156739b84a44053bfc8d9b3d9faf
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-03-22 09:14:25 -0300

    Update afk.js

--- commands/afk.js
+++ commands/afk.js
@@ -80,7 +80,7 @@ module.exports.run = async ({args, response, user, channel, client}) => {
 
 module.exports.config = {
  name: 'afk',
- aliases: ['gn', 'study', 'shower', 'food', 'work', 'poop'],
+ aliases: ['gn', 'study', 'shower', 'food', 'work', 'poop', 'game'],
  usage: 'afk',
  description: 'Seta um afk',
  cooldown: 4000,
