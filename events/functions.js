337d98ca6bc04d6380d65e60de9b7d77bac4ed78
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-04-19 10:27:21 -0300

    Update functions.js

--- events/functions.js
+++ events/functions.js
@@ -15,7 +15,7 @@ async function timedRemindCheck() {
 
   if(remindTimeCheck) {
     let {userchannel, usersender, channelsender, message, timeparsed, id} = remindTimeCheck;
-    let formatedTime = ms(Date.now() - timeparsed, {colonNotation: true});
+    let formatedTime = ms(timeparsed, {colonNotation: true});
 	  
     await db.query(`DELETE FROM luulbot_remindtimed WHERE id = '${id}'`);	  
 
