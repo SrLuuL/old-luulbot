89cf221c95b5fa394c9953b4434880814eb7741d
SrLuuL <61059616+SrLuuL@users.noreply.github.com>
2021-04-19 09:38:53 -0300

    Update reminder.js

--- commands/reminder.js
+++ commands/reminder.js
@@ -54,6 +54,7 @@ module.exports.run = async ({args, user, channel}) => {
     if(timeCheck && duration > 31536000999) return { reply: 'lembrete ultrapassa o limite de tempo' };
 
     const formatedDuration = ms(duration, {colonNotation: true});
+    reason.replace('in ' + timed, '').replace(' in ' + timed, '').replace(' in' + timed, '');
 
 
     await db.query(`INSERT INTO luulbot_remindtimed(userchannel, usersender, channelsender, message, time) VALUES($1,$2,$3,$4,$5)`, [user.username, targetUser, channel, reason, currentTime]);
